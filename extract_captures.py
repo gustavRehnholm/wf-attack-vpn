2edbaec31eaa7023695cc238b025a778ddf4bff7
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-22 14:58:28 +0100

    test

--- extract_captures.py
+++ extract_captures.py
@@ -2,12 +2,20 @@ import pandas as pd
 
 '''
 extract the captured VPN data that has been generated from the MIT project
+
+python wf-attack-vpn/extract_captures.py
 '''
 
 def main(): 
     df = pd.read_hdf("VNAT_Dataframe_release_1.h5")
 
-    print(df)
+    print("The DataFrame :")
+    display(df.head())
+
+    # print the list using tolist()
+    print("The column headers :")
+    
+    print(df.columns.tolist())
 
 if __name__=="__main__":
     main()
