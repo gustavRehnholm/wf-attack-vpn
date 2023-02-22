4d596b74ff799e9f25e96a48473fba4b3e86bb60
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-02-22 14:59:21 +0100

    Update extract_captures.py

--- extract_captures.py
+++ extract_captures.py
@@ -10,7 +10,7 @@ def main():
     df = pd.read_hdf("VNAT_Dataframe_release_1.h5")
 
     print("The DataFrame :")
-    display(df.head())
+    print(df.head())
 
     # print the list using tolist()
     print("The column headers :")
