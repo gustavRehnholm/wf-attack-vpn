624535d2b0971b9c65d16bf76c771b8eaa811734
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-03 12:54:26 +0100

    Update merge_parsed_noise.py

--- KAU-twitch-parsing/merge_parsed_noise.py
+++ KAU-twitch-parsing/merge_parsed_noise.py
@@ -17,7 +17,7 @@ def main():
 
     COL_NAMES =  ['timeframe', 'direction', 'size']
 
-    time_deviation = 0
+    deviation_time = 0
     merged_df = pd.DataFrame(columns = COL_NAMES)
 
     for file in os.listdir(DIR_NOISE):
@@ -32,17 +32,13 @@ def main():
         path = DIR_NOISE + filename
         df_file = pd.read_csv(path, names = COL_NAMES)
 
-        print(df_file.keys())
 
-        return
-
-        '''
         # time corrections on the timeframes
         for i, row in df.iterrows():
             row["timeframe"] =  row["timeframe"] + deviation_time
 
         # to shift each new capture forward in time
-        time_deviation = df['timeframe'].iloc[-1]
+        deviation_time = df['timeframe'].iloc[-1]
 
         
         merged_df = pd.concat([merged_df, df_file], axis=0)
@@ -50,7 +46,7 @@ def main():
     # Store the result in h5 file, for future use of the data
     df_file_name = DIR_PARSED_NOISE + "twitch.h5" 
     merged_df.to_hdf(df_file_name, mode = "w", key = "df")
-    '''
+
 
 # run main 
 if __name__=="__main__":
