df3ca959aefd63f02d46e463d33cba28cda008b9
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-06 08:14:54 +0100

    function template
    functions that needs to be implemented to merge the noise with the web traffic

---
+++ generate_realistic_dataset/merge_dataset_noise.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''
+This program merges the web traffic with noise, so it can be used to test WF attacks
+'''
+
+def main():
+
+    print("TODO")
+
+if __name__=="__main__":
+    main()
