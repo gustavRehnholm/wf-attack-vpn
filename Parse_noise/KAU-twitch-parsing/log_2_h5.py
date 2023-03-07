a41e7be79c16c4ab0d222f29fda178f6e2ea47b2
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-07 10:09:30 +0100

    Update log_2_h5.py
    refactoring and cleaning of old data before storing new

--- Parse_noise/KAU-twitch-parsing/log_2_h5.py
+++ Parse_noise/KAU-twitch-parsing/log_2_h5.py
@@ -14,15 +14,19 @@ import os
 def main():
     print("Start converting twitch traffic")
     # parsed noise files
-    DIR_captures = "captures/"
+    DIR_input = "captures/"
     # the captures in h5 format
-    DIR_MERGED_NOISE = "twitch/raw_captures_h5/"
+    DIR_output = "twitch/raw_captures_h5/"
 
     COL_NAMES =  ['time', 'sender', 'receiver', 'size']
 
+    # clean the prevoius content of the 
+    os.system("rm f- r- " + DIR_output)
+    os.system("mkdir " + DIR_output)
+
 
     index = 0
-    for file in os.listdir(DIR_captures):
+    for file in os.listdir(DIR_input):
 
         filename = os.fsdecode(file)
         if not filename.endswith(".log"): 
@@ -36,10 +40,10 @@ def main():
         print("converting file " + str(index) + "/1370: " + str(filename))
         print("")
 
-        path = DIR_captures + filename
+        path = DIR_input + filename
         df = pd.read_csv(path, names = COL_NAMES)
 
-        df_file_name = DIR_MERGED_NOISE + filename.rsplit('.', 1)[0] + '.h5'
+        df_file_name = DIR_output + filename.rsplit('.', 1)[0] + '.h5'
         df.to_hdf(df_file_name, mode = "w", key = "df")
 
 # run main 
