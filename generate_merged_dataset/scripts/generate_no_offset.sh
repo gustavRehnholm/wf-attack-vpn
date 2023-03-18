bdd8a69157b599f9998f7cff29d88df5452b84c3
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-18 16:02:30 +0100

    Update generate_no_offset.sh

--- generate_merged_dataset/scripts/generate_no_offset.sh
+++ generate_merged_dataset/scripts/generate_no_offset.sh
@@ -18,8 +18,12 @@
 #500
 
 python wf-attack-vpn/generate_merged_dataset/main.py
-send "foreground_traffic\r"
-send "background_traffic/twitch.h5\r"
-send "merged_traffic/twitch_no_offset/twitch_no_offset_2.5h"
-send "1000"
+sleep 1
+echo "foreground_traffic"
+sleep 1
+echo "background_traffic/twitch.h5"
+sleep 1
+echo "merged_traffic/twitch_no_offset/twitch_no_offset_2.5h"
+sleep 1
+echo "1000"
 
