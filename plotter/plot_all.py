2f94da46cb9a125c8447bcde5e8362f490323ae9
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-18 16:55:16 +0100

    Update plot_all.py

--- plotter/plot_all.py
+++ plotter/plot_all.py
@@ -10,7 +10,7 @@ python wf-attack-vpn/plotter/plot_all.py
 '''
 
 import os
-
+import sys
 from plot_df import plotDf
 
 '''
@@ -24,10 +24,9 @@ def main():
 
     labels = ["default", "constant", "tiktok"]
 
-    title = input("Where is the csv files located?")
-    csv_files = os.listdir(title)
-
-
+    path = sys.argv[1]
+    title = path.split("/")[-1]
+    csv_files = os.listdir(path)
 
     plotDf(title = title, list_of_csv = csv_files, labels = lables)
 
