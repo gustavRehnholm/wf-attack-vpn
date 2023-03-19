938046f1f6beeb7d3ed24fd6ccafa585a73b5e8a
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-19 15:42:49 +0100

    Update plot.sh

--- plotter/plot.sh
+++ plotter/plot.sh
@@ -2,5 +2,11 @@
 
 # ./wf-attack-vpn/plotter/plot.sh
 
-python wf-attack-vpn/plotter/plot_all.py twitch_no_offset wf-result/twitch/no_offset
+# navie approach
+python wf-attack-vpn/plotter/plot_all.py twitch_no_offset merged_traffic/twitch/twitch_no_offset wf-result/twitch/twitch_no_offset
 
+# offset
+python wf-attack-vpn/plotter/plot_all.py twitch_offset merged_traffic/twitch/twitch_offset wf-result/twitch/twitch_offset
+
+# rnd chunks
+python wf-attack-vpn/plotter/plot_all.py twitch_rnd merged_traffic/twitch/twitch_rnd wf-result/twitch/twitch_rnd
