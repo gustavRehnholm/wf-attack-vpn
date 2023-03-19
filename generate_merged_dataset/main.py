e4d9c194418a97b3d8f68abab33b182d41e56437
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-19 19:53:59 +0100

    Update main.py

--- generate_merged_dataset/main.py
+++ generate_merged_dataset/main.py
@@ -22,9 +22,32 @@ def main():
         print("the provided value is not an integer, aborting the program ")
         return
 
-    offset = bool(sys.argv[4])
-    rnd    = bool(sys.argv[5])
-    divide = bool(sys.argv[6])
+    if sys.argv[4] == "True":
+        offset = True
+    elif sys.argv[4] == "False":
+        offset = False
+    else:
+        print("invalid offset")
+        print(sys.argv[4])
+        return
+
+    if sys.argv[5] == "True":
+        rnd = True
+    elif sys.argv[5] == "False":
+        rnd = False
+    else:
+        print("invalid random")
+        print(sys.argv[5])
+        return
+
+    if sys.argv[6] == "True":
+        divide = True
+    elif sys.argv[6] == "False":
+        divide = False
+    else:
+        print("invalid divide")
+        print(sys.argv[6])
+        return
 
     generateMergedTraffic(dir_foreground    = DIR_FOREGROUND, 
                           dir_merged        = DIR_MERGED, 
