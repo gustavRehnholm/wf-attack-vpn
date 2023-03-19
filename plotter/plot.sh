103001d0e2c68c81b21eb37689d1890d6da425d6
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-19 16:07:58 +0100

    typo

--- plotter/plot.sh
+++ plotter/plot.sh
@@ -3,10 +3,12 @@
 # ./wf-attack-vpn/plotter/plot.sh
 
 # navie approach
-python wf-attack-vpn/plotter/plot_all.py twitch_no_offset wf-result/twitch/twitch_no_offset wf-result/twitch/twitch_no_offset
+python wf-attack-vpn/plotter/plot_all.py twitch_no_offset_2600h wf-result/twitch/twitch_no_offset/twitch_no_offset_2600h wf-result/twitch/twitch_no_offset/twitch_no_offset_2600h
+python wf-attack-vpn/plotter/plot_all.py twitch_no_offset_5h    wf-result/twitch/twitch_no_offset/twitch_no_offset_5h    wf-result/twitch/twitch_no_offset/twitch_no_offset_5h
+python wf-attack-vpn/plotter/plot_all.py twitch_no_offset_2_5h  wf-result/twitch/twitch_no_offset/twitch_no_offset_2_5h  wf-result/twitch/twitch_no_offset/twitch_no_offset_2_5h
 
 # offset
-python wf-attack-vpn/plotter/plot_all.py twitch_offset wf-result/twitch/twitch_offset wf-result/twitch/twitch_offset
+#python wf-attack-vpn/plotter/plot_all.py twitch_offset wf-result/twitch/twitch_offset wf-result/twitch/twitch_offset
 
 # rnd chunks
-python wf-attack-vpn/plotter/plot_all.py twitch_rnd_no_offset wf-result/twitch/twitch_rnd_no_offset wf-result/twitch/twitch_rnd_no_offset
+#python wf-attack-vpn/plotter/plot_all.py twitch_rnd_no_offset wf-result/twitch/twitch_rnd_no_offset wf-result/twitch/twitch_rnd_no_offset
