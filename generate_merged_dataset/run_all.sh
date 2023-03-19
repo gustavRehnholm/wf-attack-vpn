d05c153bf09ffa8a60685c2bfe368e057c732d07
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-19 17:42:26 +0100

    Update run_all.sh

--- generate_merged_dataset/run_all.sh
+++ generate_merged_dataset/run_all.sh
@@ -10,6 +10,6 @@
 #python wf-attack-vpn/generate_merged_dataset/main.py foreground_traffic background_traffic/twitch.h5 merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_2_5h  1000
 
 # rnd with offset
-python wf-attack-vpn/generate_merged_dataset/main.py foreground_traffic background_traffic/twitch.h5 merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_2600h 1    True True
-python wf-attack-vpn/generate_merged_dataset/main.py foreground_traffic background_traffic/twitch.h5 merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_5h    500  True True
-python wf-attack-vpn/generate_merged_dataset/main.py foreground_traffic background_traffic/twitch.h5 merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_2_5h  1000 True True
+python wf-attack-vpn/generate_merged_dataset/main.py foreground_traffic background_traffic/twitch.h5 merged_traffic/twitch_rnd_offset/twitch_rnd_offset_2600h 1    True True
+python wf-attack-vpn/generate_merged_dataset/main.py foreground_traffic background_traffic/twitch.h5 merged_traffic/twitch_rnd_offset/twitch_rnd_offset_5h    500  True True
+python wf-attack-vpn/generate_merged_dataset/main.py foreground_traffic background_traffic/twitch.h5 merged_traffic/twitch_rnd_offset/twitch_rnd_offset_2_5h  1000 True True
