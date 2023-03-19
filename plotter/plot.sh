3ac83aa928b9b26349514cff1c1b18966da83efe
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-19 16:13:28 +0100

    Update plot.sh

--- plotter/plot.sh
+++ plotter/plot.sh
@@ -3,12 +3,16 @@
 # ./wf-attack-vpn/plotter/plot.sh
 
 # navie approach
-python wf-attack-vpn/plotter/plot_all.py twitch_no_offset_2600h wf-result/twitch/twitch_no_offset/twitch_no_offset_2600h 
-python wf-attack-vpn/plotter/plot_all.py twitch_no_offset_5h    wf-result/twitch/twitch_no_offset/twitch_no_offset_5h    
-python wf-attack-vpn/plotter/plot_all.py twitch_no_offset_2_5h  wf-result/twitch/twitch_no_offset/twitch_no_offset_2_5h  
+#python wf-attack-vpn/plotter/plot_all.py twitch_no_offset_2600h wf-result/twitch/twitch_no_offset/twitch_no_offset_2600h fig/twitch_no_offset
+#python wf-attack-vpn/plotter/plot_all.py twitch_no_offset_5h    wf-result/twitch/twitch_no_offset/twitch_no_offset_5h    fig/twitch_no_offset
+#python wf-attack-vpn/plotter/plot_all.py twitch_no_offset_2_5h  wf-result/twitch/twitch_no_offset/twitch_no_offset_2_5h  fig/twitch_no_offset
 
 # offset
-#python wf-attack-vpn/plotter/plot_all.py twitch_offset wf-result/twitch/twitch_offset wf-result/twitch/twitch_offset
+#python wf-attack-vpn/plotter/plot_all.py twitch_offset_2600h wf-result/twitch/twitch_offset/twitch_offset_2600h fig/twitch_offset
+#python wf-attack-vpn/plotter/plot_all.py twitch_offset_5h    wf-result/twitch/twitch_offset/twitch_offset_5h    fig/twitch_offset
+#python wf-attack-vpn/plotter/plot_all.py twitch_offset_2_5h  wf-result/twitch/twitch_offset/twitch_offset_2_5h  fig/twitch_offset
 
 # rnd chunks
-#python wf-attack-vpn/plotter/plot_all.py twitch_rnd_no_offset wf-result/twitch/twitch_rnd_no_offset wf-result/twitch/twitch_rnd_no_offset
+python wf-attack-vpn/plotter/plot_all.py twitch_rnd_no_offset_2600h wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_2600h fig/twitch_rnd_no_offset
+python wf-attack-vpn/plotter/plot_all.py twitch_rnd_no_offset_5h    wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_5h    fig/twitch_rnd_no_offset
+python wf-attack-vpn/plotter/plot_all.py twitch_rnd_no_offset_2_5h  wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_2_5h  fig/twitch_rnd_no_offset
