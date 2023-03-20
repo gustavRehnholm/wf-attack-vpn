f750161cf10e8d098a93adc91948f9cee06aa869
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-20 23:57:53 +0100

    Update wf-attack-100.sh

--- wf-attack/wf-attack-100.sh
+++ wf-attack/wf-attack-100.sh
@@ -3,9 +3,9 @@
 # ./wf-attack-vpn/wf-attack/wf-attack-100.sh
 
 # rnd-no offset
-python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_2600h wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_2600h 100
-python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_5h    wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_5h    100
-python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_2_5h  wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_2_5h  100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_2600h wf-result/twitch_rnd_no_offset/twitch_rnd_no_offset_2600h 100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_5h    wf-result/twitch_rnd_no_offset/twitch_rnd_no_offset_5h    100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_2_5h  wf-result/twitch_rnd_no_offset/twitch_rnd_no_offset_2_5h  100
 
 # rnd-offset
 python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_offset/twitch_rnd_offset_2600h wf-result/twitch_rnd_offset/twitch_rnd_offset_2600h 100
