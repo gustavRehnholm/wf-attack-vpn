7feb4a905e6b199bedbf017314e462e18e8b7dc3
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-20 22:00:50 +0100

    Update wf-attack.sh

--- wf-attack/wf-attack.sh
+++ wf-attack/wf-attack.sh
@@ -3,19 +3,19 @@
 # ./wf-attack-vpn/wf-attack/wf-attack.sh
 
 # ittr-no offset
-#python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_no_offset/twitch_ittr_no_offset_2600h wf-result/twitch/twitch_ittr_no_offset/twitch_no_offset_2600h
-python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_no_offset/twitch_ittr_no_offset_5h    wf-result/twitch/twitch_ittr_no_offset/twitch_no_offset_5h
-python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_no_offset/twitch_ittr_no_offset_2_5h  wf-result/twitch/twitch_ittr_no_offset/twitch_no_offset_2.5h
+#python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_no_offset/twitch_ittr_no_offset_2600h wf-result/twitch/twitch_ittr_no_offset/twitch_ittr_no_offset_2600h
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_no_offset/twitch_ittr_no_offset_5h    wf-result/twitch/twitch_ittr_no_offset/twitch_ittr_no_offset_5h
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_no_offset/twitch_ittr_no_offset_2_5h  wf-result/twitch/twitch_ittr_no_offset/twitch_ittr_no_offset_2_5h
 
 # ittr-offset
-#python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_offset/twitch_offset_2600h wf-result/twitch/twitch_ittr_no_offset/twitch_no_offset_2600h
-#python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_offset/twitch_offset_5h    wf-result/twitch/twitch_ittr_no_offset/twitch_no_offset_5h
-#python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_offset/twitch_offset_2_5h  wf-result/twitch/twitch_ittr_no_offset/twitch_no_offset_2.5h
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_offset/twitch_ittr_offset_2600h wf-result/twitch/twitch_ittr_offset/twitch_ittr_offset_2600h
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_offset/twitch_ittr_offset_5h    wf-result/twitch/twitch_ittr_offset/twitch_ittr_offset_5h
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_ittr_offset/twitch_ittr_offset_2_5h  wf-result/twitch/twitch_ittr_offset/twitch_ittr_offset_2_5h
 
 # rnd-no offset
-#python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_2600h wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_2600h
-#python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_5h    wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_5h
-#python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_2_5h  wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_2_5h
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_2600h wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_2600h
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_5h    wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_5h
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_no_offset/twitch_rnd_no_offset_2_5h  wf-result/twitch/twitch_rnd_no_offset/twitch_rnd_no_offset_2_5h
 
 # rnd-offset
 python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_offset/twitch_rnd_offset_2600h wf-result/twitch_rnd_offset/twitch_rnd_offset_2600h
