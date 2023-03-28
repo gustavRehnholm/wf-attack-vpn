35c4e55f5e5ec40a3cfe8db8e0fe16445331b7e9
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-28 14:37:46 +0200

    Update get_undefended.sh

--- undefended/get_undefended.sh
+++ undefended/get_undefended.sh
@@ -3,7 +3,7 @@
 # 10-fold on the undefended dataset
 # ./wf-attack-vpn/undefended/get_undefended.sh
 
-./df-fitness.py -d foreground_traffic/client --train -s 100 -f 1 --csv wf-result/undef/fold1.csv
+#./df-fitness.py -d foreground_traffic/client --train -s 100 -f 1 --csv wf-result/undef/fold1.csv
 ./df-fitness.py -d foreground_traffic/client --train -s 100 -f 2 --csv wf-result/undef/fold2.csv
 ./df-fitness.py -d foreground_traffic/client --train -s 100 -f 3 --csv wf-result/undef/fold3.csv
 ./df-fitness.py -d foreground_traffic/client --train -s 100 -f 4 --csv wf-result/undef/fold4.csv
@@ -14,6 +14,6 @@
 ./df-fitness.py -d foreground_traffic/client --train -s 100 -f 9 --csv wf-result/undef/fold9.csv
 ./df-fitness.py -d foreground_traffic/client --train -s 100 -f 0 --csv wf-result/undef/fold0.csv
 
-python wf-attack-vpn/undefended/merge.py
+#python wf-attack-vpn/undefended/merge.py
 
-python wf-attack-vpn/plotter/plot_all.py undefended wf-result/undef fig/undef
+#python wf-attack-vpn/plotter/plot_all.py undefended wf-result/undef fig/undef
