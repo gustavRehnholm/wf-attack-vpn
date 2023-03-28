cf1dcb3e4460ce05940d0724abce85d893833fd7
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-28 17:19:18 +0200

    to run for a smaller datasample

--- parse_background/KAU-twitch-parsing/merge_parsed_noise.py
+++ parse_background/KAU-twitch-parsing/merge_parsed_noise.py
@@ -14,7 +14,7 @@ def main():
     DIR_INPUT = "twitch/parsed_captures/"
     # the merged noise file in the h5 format
     DIR_OUTPUT = "background_traffic"
-    FILE_OUTPUT = "twitch.h5"
+    FILE_OUTPUT = "twitch_200.h5"
     PATH_OUTPUT = DIR_OUTPUT + "/" + FILE_OUTPUT
     # for storing the result as h5
     key = "df"
@@ -59,8 +59,8 @@ def main():
         df.to_hdf(PATH_OUTPUT, mode = "r+", key = key, append = True) 
 
         # to gather a subset
-        #if index > 200:
-        #    return
+        if index > 200:
+            return
 
 
     print("Have merged all twitch traffic, store them now in " + PATH_OUTPUT)
