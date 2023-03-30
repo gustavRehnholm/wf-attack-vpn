f8217c09b7481cbab58e2613307adf540f8d0476
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-30 11:02:44 +0200

    refactoring

--- merge_traffic/get_merged.py
+++ merge_traffic/get_merged.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 
 '''
-This program merges the web traffic with noise, so it can be used to test WF attacks
-
-If the program runs out of primary memory during its run time, test to make the variable chunk smaller
-
-python wf-attack-vpn/generate_merged_dataset/get_merged.py
+To run:
+python wf-attack-vpn/merged_traffic/main.py
 '''
 
 import pandas as pd
 import os
 from merge_traffic import mergeTraffic
+import shutil
 
 '''
+This program merges the inputted background and foreground traffic, and store it in the specified directory.
+One can if one which to specify which fold file to use, to split the foreground traffic differently.
+
 Input:
     dir_foreground: path to the directory that has the foreground that the background should be merged into
-    dir_merged: path to the directory where the merged result will be stored in
+    dir_merged:     path to the directory where the merged result will be stored in
     dir_background: path to the directory where the background traffic is
     fold:           which fold file to use
+
+Output:
+    Boolean which tells if the program succeeded in creating the merged traffic set
 '''
 def getMerged(dir_foreground, dir_merged, dir_background, fold = 0):
 
@@ -32,44 +36,40 @@ def getMerged(dir_foreground, dir_merged, dir_background, fold = 0):
     mergedValidFiles = []
     mergedTestFiles  = []
 
-
-    print("Start the test of the merging of foreground and background traffic")
-
-    # clean the previous result
-    os.system("rm -f -r " + dir_merged)
-    os.system("mkdir " + dir_merged)
-
-
+    # clean old results if their is any
+    if os.path.exists(dir_merged):
+        shutil.rmtree(dir_merged)
+    os.mkdir(dir_merged)
+    
     # The naming structure between the foreground and the merged should be the same
     for (dirpath, dirnames, filenames) in os.walk(dir_foreground, topdown=True):
         for dirs in dirnames:
-            try: 
-                os.mkdir(os.path.join(dir_merged, dirs))
-            except: 
-                print("File and directory exists!") 
+            curr_path = os.path.join(dir_merged, dirs)
+            if not os.path.exists(os.path.join(dir_merged, dirs)):
+                os.mkdir(curr_path)
 
 
     # dataframe representation of what each foreground file should be used for
-    df = pd.read_csv(FOLD_CSV)
-    FOLD_DF_ATTR = ['log', 'is_train', 'is_valid', 'is_test']
-    dfFiles = df[FOLD_DF_ATTR]
+    df_fold = pd.read_csv(FOLD_CSV)
+    #FOLD_DF_ATTR = ['log', 'is_train', 'is_valid', 'is_test']
+    #dfFiles = df_fold[FOLD_DF_ATTR]
 
     # For every log file in the web traffic, make sure that there is an correlating log file to store the parsed result
-    for x in range(0, len(dfFiles['log'])):
-        if(dfFiles['is_train'][x] == True): 
-            mergedTrainFiles.append(os.path.join(dir_merged, dfFiles['log'][x]))
-            foregroundTrainFiles.append(os.path.join(dir_foreground, "client", dfFiles['log'][x]))
+    for x in range(0, len(df_fold['log'])):
+        if(df_fold['is_train'][x] == True): 
+            mergedTrainFiles.append(os.path.join(dir_merged, df_fold['log'][x]))
+            foregroundTrainFiles.append(os.path.join(dir_foreground, "client", df_fold['log'][x]))
 
-        elif(dfFiles['is_valid'][x] == True): 
-            mergedValidFiles.append(os.path.join(dir_merged, dfFiles['log'][x]))
-            foregroundValidFiles.append(os.path.join(dir_foreground, "client", dfFiles['log'][x]))
+        elif(df_fold['is_valid'][x] == True): 
+            mergedValidFiles.append(os.path.join(dir_merged, df_fold['log'][x]))
+            foregroundValidFiles.append(os.path.join(dir_foreground, "client", df_fold['log'][x]))
 
-        elif(dfFiles['is_test'][x] == True): 
-            mergedTestFiles.append(os.path.join(dir_merged, dfFiles['log'][x]))
-            foregroundTestFiles.append(os.path.join(dir_foreground, "client", dfFiles['log'][x]))
+        elif(df_fold['is_test'][x] == True): 
+            mergedTestFiles.append(os.path.join(dir_merged, df_fold['log'][x]))
+            foregroundTestFiles.append(os.path.join(dir_foreground, "client", df_fold['log'][x]))
 
         else:
-            print("ERROR: the file " + dfFiles['log'][x] + "does not have a determined usage")
+            print("ERROR: the file " + df_fold['log'][x] + "does not have a determined usage")
             print("Aborting program")
             return
 
@@ -83,15 +83,18 @@ def getMerged(dir_foreground, dir_merged, dir_background, fold = 0):
     print("Size of the background")
     print(df_len)
 
+    return True
+
     if not mergeTraffic(mergedTestFiles , foregroundTestFiles , dir_background, 0              , part_of_10):
-        return
+        return False
     if not mergeTraffic(mergedValidFiles, foregroundValidFiles, dir_background, part_of_10 + 1 , part_of_10*2):
-        return
+        return False
     if not mergeTraffic(mergedTrainFiles, foregroundTrainFiles, dir_background, part_of_10*2 + 1, int(df_len)):
-        return
+        return False
 
 
     print("Succeeded in creating the merged traffic set")
+    return True
 
 if __name__=="__main__":
     main()
--- merge_traffic/main.py
+++ merge_traffic/main.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
 '''
-This program creates the merged traffic, used to test the WF attacks capabilities
-
+To run:
 python wf-attack-vpn/merge_traffic/main.py
 '''
 
+# to access input from the user
 import sys
+# to run the script for the actually merging
 from get_merged import getMerged
 
+'''
+Creates the merged traffic, used to test the WF attacks capabilities
+Input:
+    sys.argv[1]: the directory where the foreground dataset is stored
+    sys.argv[2]: the directory where the background dataset is stored
+    sys.argv[3]: the directory where the merged dataset will be stored
+    sys.argv[4]: Which fold file to use, should be an integer from 0 to 9 
+'''
 def main():
 
     DIR_FOREGROUND = sys.argv[1]
@@ -24,10 +33,17 @@ def main():
     print("Chosen fold")
     print(sys.argv[4])
 
-    getMerged(dir_foreground    = DIR_FOREGROUND, 
-              dir_merged        = DIR_MERGED, 
-              dir_background    = DIR_BACKGROUND,
-              fold              = FOLD)
+    success = getMerged(dir_foreground    = DIR_FOREGROUND, 
+                        dir_merged        = DIR_MERGED, 
+                        dir_background    = DIR_BACKGROUND,
+                        fold              = FOLD)
+
+    if success:
+        print("The merged traffic was created successfully")
+    else:
+        print("ERROR: the merged traffic could not be created")
+
+    return
 
 if __name__=="__main__":
     main()
