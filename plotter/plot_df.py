7fdf5e3f148486a490d152a0da2c7a1b5952bb46
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-31 08:57:31 +0200

    Update plot_df.py

--- plotter/plot_df.py
+++ plotter/plot_df.py
@@ -51,9 +51,9 @@ def plotDf(title = "untitled", list_of_csv = [], labels = [], result_path = "fig
     for j in range(0, len(datasets)):
         ax = sns.pointplot(data = datasets[j][["th", "accuracy"]], x ="th", y="accuracy", markers = markers_list[j], color = colors[j], label = labels[j])
 
-    #x_ticks = [0.1, 0.5, 0.7, 0.9]
+    x_ticks = [0.1, 0.5, 0.7, 0.9]
     #labels_x = ["0.1", "0.5", "0.7", "0.9"]
-    #plt.xticks(ticks = x_ticks, rotation ='horizontal')
+    plt.xticks(ticks = x_ticks, rotation ='horizontal')
 
     #ax.set_xticks(ticks = x_ticks)
     #ax.set_xticklabels(labels_x)
