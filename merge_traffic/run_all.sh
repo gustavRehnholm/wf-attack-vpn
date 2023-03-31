ebb1f97b124476ccc11b60a650a212a90b71e656
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-03-31 08:13:39 +0200

    comments

--- merge_traffic/run_all.sh
+++ merge_traffic/run_all.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# To create all the merged sets from the twitch dataset.
+# the input is ordered as:
 # {foreground} {background} {merged} {fold}
 
+# to run:
 # ./wf-attack-vpn/merge_traffic/run_all.sh
 
 python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_150.h5 merged_traffic/twitch_div_rnd_150 1
 python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_1.h5   merged_traffic/twitch_div_rnd_1   1
+
+python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_150.h5 merged_traffic/twitch_rnd_150 1
+python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_1.h5   merged_traffic/twitch_rnd_1   1
