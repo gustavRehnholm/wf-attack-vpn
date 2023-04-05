9d656db3e9823ca876adf5202df4633b0b0bf270
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-05 08:28:58 +0200

    Update plot.sh

--- plotter/plot.sh
+++ plotter/plot.sh
@@ -11,15 +11,6 @@ python wf-attack-vpn/plotter/plot_all.py twitch_old_og  wf-result/twitch_old/old
 python wf-attack-vpn/plotter/plot_all.py twitch_old_20  wf-result/twitch_old/old_twitch_20  fig/twitch_old
 python wf-attack-vpn/plotter/plot_all.py twitch_old_100 wf-result/twitch_old/old_twitch_100 fig/twitch_old
 
-# The final comparison graph
-python wf-attack-vpn/plotter/plot_all.py compare_20_constant  wf-result/compare_20_constant  fig/compare_20_constant
-python wf-attack-vpn/plotter/plot_all.py compare_20_default   wf-result/compare_20_default   fig/compare_20_default
-python wf-attack-vpn/plotter/plot_all.py compare_20_tiktok    wf-result/compare_20_tiktok    fig/compare_20_tiktok
-
-python wf-attack-vpn/plotter/plot_all.py compare_100_constant  wf-result/compare_100_constant  fig/compare_100_constant
-python wf-attack-vpn/plotter/plot_all.py compare_100_default   wf-result/compare_100_default   fig/compare_100_default
-python wf-attack-vpn/plotter/plot_all.py compare_100_tiktok    wf-result/compare_100_tiktok    fig/compare_100_tiktok
-
 # offset. sample 20
 python wf-attack-vpn/plotter/plot_all.py twitch_offset_2600h wf-result/twitch_offset/twitch_offset_2600h fig/twitch_offset
 python wf-attack-vpn/plotter/plot_all.py twitch_offset_2_5h  wf-result/twitch_offset/twitch_offset_2_5h  fig/twitch_offset
@@ -44,3 +35,51 @@ python wf-attack-vpn/plotter/plot_all.py twitch_div_rnd_20_2h   wf-result/twitch
 # Divided, randomized, sample 100
 python wf-attack-vpn/plotter/plot_all.py twitch_div_rnd_100_300h wf-result/twitch_div_rnd_100/twitch_div_rnd_300h fig/twitch_div_rnd
 python wf-attack-vpn/plotter/plot_all.py twitch_div_rnd_100_2h   wf-result/twitch_div_rnd_100/twitch_div_rnd_2h   fig/twitch_div_rnd
+
+# cp files for the comparison
+
+# 20 default
+cp wf-result/undef_20/combined/combined.csv                      wf-result/compare_20_default/undef.csv
+cp wf-result/twitch_old/old_twitch_20/default.csv                wf-result/compare_20_default/twitch.csv
+cp wf-result/twitch_div_ittr_20/twitch_div_ittr_2_5h/default.csv wf-result/compare_20_default/ittr.csv
+cp wf-result/twitch_div_rnd_20/twitch_div_rnd_2h/default.csv     wf-result/compare_20_default/rnd.csv
+
+# 20 constant
+cp wf-result/undef_20/combined/combined.csv                       wf-result/compare_20_constant/undef.csv
+cp wf-result/twitch_old/old_twitch_20/constant.csv                wf-result/compare_20_constant/twitch.csv
+cp wf-result/twitch_div_ittr_20/twitch_div_ittr_2_5h/constant.csv wf-result/compare_20_constant/ittr.csv
+cp wf-result/twitch_div_rnd_20/twitch_div_rnd_2h/constant.csv     wf-result/compare_20_constant/rnd.csv
+
+# 20 Tik-Tok
+cp wf-result/undef_20/combined/combined.csv                     wf-result/compare_20_tiktok/undef.csv
+cp wf-result/twitch_old/old_twitch_20/tiktok.csv                wf-result/compare_20_tiktok/twitch.csv
+cp wf-result/twitch_div_ittr_20/twitch_div_ittr_2_5h/tiktok.csv wf-result/compare_20_tiktok/ittr.csv
+cp wf-result/twitch_div_rnd_20/twitch_div_rnd_2h/tiktok.csv     wf-result/compare_20_tiktok/rnd.csv
+
+# 100 default
+cp wf-result/undef_100/combined/combined.csv                      wf-result/compare_100_default/undef.csv
+cp wf-result/twitch_old/old_twitch_100/default.csv                wf-result/compare_100_default/twitch.csv
+cp wf-result/twitch_div_ittr_100/twitch_div_ittr_2_5h/default.csv wf-result/compare_100_default/ittr.csv
+cp wf-result/twitch_div_rnd_100/twitch_div_rnd_2h/default.csv     wf-result/compare_100_default/rnd.csv
+
+# 100 constant
+cp wf-result/undef_100/combined/combined.csv                       wf-result/compare_100_constant/undef.csv
+cp wf-result/twitch_old/old_twitch_100/constant.csv                wf-result/compare_100_constant/twitch.csv
+cp wf-result/twitch_div_ittr_100/twitch_div_ittr_2_5h/constant.csv wf-result/compare_100_constant/ittr.csv
+cp wf-result/twitch_div_rnd_100/twitch_div_rnd_2h/constant.csv     wf-result/compare_100_constant/rnd.csv
+
+# 100 Tik-Tok
+cp wf-result/undef_100/combined/combined.csv                     wf-result/compare_100_tiktok/undef.csv
+cp wf-result/twitch_old/old_twitch_100/tiktok.csv                wf-result/compare_100_tiktok/twitch.csv
+cp wf-result/twitch_div_ittr_100/twitch_div_ittr_2_5h/tiktok.csv wf-result/compare_100_tiktok/ittr.csv
+cp wf-result/twitch_div_rnd_100/twitch_div_rnd_2h/tiktok.csv     wf-result/compare_100_tiktok/rnd.csv
+
+
+# The final comparison graph
+python wf-attack-vpn/plotter/plot_all.py compare_20_constant  wf-result/compare_20_constant  fig/compare_20_constant
+python wf-attack-vpn/plotter/plot_all.py compare_20_default   wf-result/compare_20_default   fig/compare_20_default
+python wf-attack-vpn/plotter/plot_all.py compare_20_tiktok    wf-result/compare_20_tiktok    fig/compare_20_tiktok
+
+python wf-attack-vpn/plotter/plot_all.py compare_100_constant  wf-result/compare_100_constant  fig/compare_100_constant
+python wf-attack-vpn/plotter/plot_all.py compare_100_default   wf-result/compare_100_default   fig/compare_100_default
+python wf-attack-vpn/plotter/plot_all.py compare_100_tiktok    wf-result/compare_100_tiktok    fig/compare_100_tiktok
