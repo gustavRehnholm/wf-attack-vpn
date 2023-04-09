48e1e9a5f72008d4cb156b65b7fc6a3ce4d2379b
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-09 17:13:27 +0200

    Update plot.sh

--- plotter/plot.sh
+++ plotter/plot.sh
@@ -23,19 +23,19 @@ python wf-attack-vpn/plotter/plot_all.py twitch_div_rnd_100_2h   wf-result/twitc
 # 100 default
 cp wf-result/undef_100/combined/combined.csv                      wf-result/compare_100_default/undef.csv
 cp wf-result/twitch_old/old_twitch_100/default.csv                wf-result/compare_100_default/twitch.csv
-cp wf-result/twitch_rnd_20/twitch_rnd_2h/default.csv              wf-result/compare_100_default/rnd.csv
+cp wf-result/twitch_rnd_100/twitch_rnd_2h/default.csv              wf-result/compare_100_default/rnd.csv
 cp wf-result/twitch_div_rnd_100/twitch_div_rnd_2h/default.csv     wf-result/compare_100_default/div-rnd.csv
 
 # 100 constant
 cp wf-result/undef_100/combined/combined.csv                       wf-result/compare_100_constant/undef.csv
 cp wf-result/twitch_old/old_twitch_100/constant.csv                wf-result/compare_100_constant/twitch.csv
-cp wf-result/twitch_rnd_20/twitch_rnd_2h/constant.csv              wf-result/compare_100_constant/rnd.csv
+cp wf-result/twitch_rnd_100/twitch_rnd_2h/constant.csv              wf-result/compare_100_constant/rnd.csv
 cp wf-result/twitch_div_rnd_100/twitch_div_rnd_2h/constant.csv     wf-result/compare_100_constant/div-rnd.csv
 
 # 100 Tik-Tok
 cp wf-result/undef_100/combined/combined.csv                     wf-result/compare_100_tiktok/undef.csv
 cp wf-result/twitch_old/old_twitch_100/tiktok.csv                wf-result/compare_100_tiktok/twitch.csv
-cp wf-result/twitch_rnd_20/twitch_rnd_2h/tiktok.csv              wf-result/compare_100_tiktok/rnd.csv
+cp wf-result/twitch_rnd_100/twitch_rnd_2h/tiktok.csv              wf-result/compare_100_tiktok/rnd.csv
 cp wf-result/twitch_div_rnd_100/twitch_div_rnd_2h/tiktok.csv     wf-result/compare_100_tiktok/div-rnd.csv
 
 # The final comparison graph
