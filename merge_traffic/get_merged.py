f6f24e06fd1d4f519d8ee28a371341bb237c8011
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-09 09:58:55 +0200

    Update get_merged.py

--- merge_traffic/get_merged.py
+++ merge_traffic/get_merged.py
@@ -78,11 +78,11 @@ def getMerged(dir_foreground, dir_merged, dir_background, fold = 0):
     print("Size of the background")
     print(df_len)
     
-    
+    '''
     print("Start merging test files")
     if not mergeTraffic(mergedTestFiles , foregroundTestFiles , dir_background, 0              , part_of_10):
         return False
-    print("Start validation test files")
+    print("Start merging validation files")
     if not mergeTraffic(mergedValidFiles, foregroundValidFiles, dir_background, part_of_10 + 1 , part_of_10*2):
         return False
     print("Start merging train files")
@@ -95,7 +95,7 @@ def getMerged(dir_foreground, dir_merged, dir_background, fold = 0):
         return False
     if not mergeTraffic(mergedTrainFiles, foregroundTrainFiles, dir_background, 0              , int(df_len)):
         return False
-    '''
+    
     print("Succeeded in creating the merged traffic set")
     return True
 
