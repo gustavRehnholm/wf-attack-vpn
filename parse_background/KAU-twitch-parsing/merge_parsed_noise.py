68bcc3df86f40ba9a771eae1dfd8a7c8ff6b15eb
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-09 12:31:35 +0200

    sort captures after size

--- parse_background/KAU-twitch-parsing/merge_parsed_noise.py
+++ parse_background/KAU-twitch-parsing/merge_parsed_noise.py
@@ -6,6 +6,7 @@ Merge all the parsed captures to one single file, where the timestamps flows log
 
 import pandas as pd
 import os
+import operator
 
 def main():
     print("Start merging the twitch captures")
@@ -14,27 +15,25 @@ def main():
     DIR_INPUT = "twitch/parsed_captures/"
     # the merged noise file in the h5 format
     DIR_OUTPUT = "background_traffic"
-    FILE_OUTPUT = "twitch_1.h5"
+    FILE_OUTPUT = "twitch_100.h5"
     PATH_OUTPUT = DIR_OUTPUT + "/" + FILE_OUTPUT
+    COL_NAMES =  ['time', 'direction', 'size']
     # for storing the result as h5
     key = "df"
     index = 0
-    df_merged = pd.DataFrame(columns = ['time', 'direction', 'size'])
-
-    COL_NAMES =  ['time', 'direction', 'size']
-
+    df_merged = pd.DataFrame(columns = COL_NAMES)
 
     # clean the previous result
     os.system("mkdir " + DIR_OUTPUT)
     os.system("rm " + PATH_OUTPUT)
     
-
     # to correct each captures time, so they all follow a chronological order
     deviation_time = 0
 
     # list of all twitch traffic captures files
     files = os.listdir(DIR_INPUT)
-    files.sort()
+    #files.sort()
+    sorted_list = sorted(files, key=os.path.getsize, reverse=True)
     files_len = len(files)
 
     # create the file, that the final result will be stored in
@@ -51,21 +50,16 @@ def main():
         print("")
         print("merging file " + str(index) + "/" + str(files_len) + ": " + str(filename))
         print("")
+        return
 
         path = DIR_INPUT + filename
         df = pd.read_hdf(path, key=key)
 
-        if filename == "tshark_1.9h_twitch-jackport99_stream_2023-01-04_1.h5":
-            # append current files result to the final result file
-            # append = True: the value should be appended on the old
-            df.to_hdf(PATH_OUTPUT, mode = "r+", key = key, append = True) 
-            print("Added the file")
-            return
+        df.to_hdf(PATH_OUTPUT, mode = "r+", key = key, append = True) 
 
         # to gather a subset
-        #if index >= 150:
-        #    return
-
+        if index >= 100:
+            return
 
     print("Have merged all twitch traffic, store them now in " + PATH_OUTPUT)
 
