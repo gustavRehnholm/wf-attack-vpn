34f52b49cc7135e844ff3f7c8cc85c5e603c6556
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-09 12:41:02 +0200

    Update merge_parsed_noise.py

--- parse_background/KAU-twitch-parsing/merge_parsed_noise.py
+++ parse_background/KAU-twitch-parsing/merge_parsed_noise.py
@@ -36,7 +36,7 @@ def main():
 
     # Sort list of file names by size 
     sorted_files = sorted(files, key =  lambda x: os.stat(os.path.join(DIR_INPUT, x)).st_size)
-    sorted_files = reversed(sorted_files)
+    sorted_files = list(reversed(sorted_files))
     files_len = len(sorted_files)
 
     # create the file, that the final result will be stored in
