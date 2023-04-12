f7ab70865710fe73d77e3495edcb1948ab84328d
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-12 08:05:43 +0200

    corrected fold

--- merge_traffic/run_all.sh
+++ merge_traffic/run_all.sh
@@ -6,11 +6,11 @@
 # to run:
 # ./wf-attack-vpn/merge_traffic/run_all.sh
 
-python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_50.h5 merged_traffic/twitch_div_rnd_50 1
-python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_1.h5  merged_traffic/twitch_div_rnd_1   1
+python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_50.h5 merged_traffic/twitch_div_rnd_50 0
+python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_1.h5  merged_traffic/twitch_div_rnd_1  0
 
-python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_50.h5 merged_traffic/twitch_rnd_50 1
-python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_1.h5  merged_traffic/twitch_rnd_1   1
+python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_50.h5 merged_traffic/twitch_rnd_50 0
+python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_1.h5  merged_traffic/twitch_rnd_1  0
 
 # wf attack
 python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_50 wf-result/twitch_rnd_100/twitch_rnd_100h 100
