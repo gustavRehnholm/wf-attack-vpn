0c68245a3f6168a8b8ba00b194e99922408a8220
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-12 16:45:59 +0200

    Update merged-dataset-stats.py

--- merged-dataset-stats.py
+++ merged-dataset-stats.py
@@ -44,7 +44,7 @@ def main():
     multiple_clients_file  = []
     one_client             = []
 
-    print(type(results[0].keys()))
+    print(results[0].keys())
     for result in results:
         if result.get(success) == True:
             # only analyse files that have one client
