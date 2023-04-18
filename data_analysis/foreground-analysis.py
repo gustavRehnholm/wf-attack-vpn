383e33adc7dd8f0a40187eaf579fbec9e32d3742
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-18 10:09:45 +0200

    Update foreground-analysis.py

--- data_analysis/foreground-analysis.py
+++ data_analysis/foreground-analysis.py
@@ -45,11 +45,7 @@ def main():
     p = Pool(args["w"])
 
     results = p.starmap(parse_trace, todo)
-    print(results)
-    sys.exit()
-    # store statistics gathered from the file
     pkt_sec = get_pkt_sec(results)
-
     plot_bar(description_text = "mean pkt/sec", x_txt = "time (s)", y_txt = "packets", stat = pkt_sec)
 
     print("Saved the result")
