5cb73bf71251f31c56586c70b661a756a8eeffb0
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-20 10:45:13 +0200

    Update log_2_h5.py

--- parse_background/KAU-twitch-parsing/log_2_h5.py
+++ parse_background/KAU-twitch-parsing/log_2_h5.py
@@ -6,12 +6,7 @@ import sys
 from multiprocessing import Pool
 import timeit
 
-COL_NAMES =  ['time', 'sender_receiver', 'size']
-# parsed noise files
-DIR_INPUT = "captures/"
-# the captures in h5 format
-DIR_OUTPUT = "twitch/raw_captures_h5/"
-KEY = "df"
+
 
 def main():
     '''
@@ -21,6 +16,12 @@ def main():
 
     print("Start converting twitch traffic")
 
+    COL_NAMES =  ['time', 'sender_receiver', 'size']
+    # parsed noise files
+    DIR_INPUT = "captures/"
+    # the captures in h5 format
+    DIR_OUTPUT = "twitch/raw_captures_h5/"
+
     # clean the previous result
     os.system("rm -f -r " + DIR_OUTPUT)
     os.system("mkdir " + DIR_OUTPUT)
@@ -28,7 +29,7 @@ def main():
     input = []
     files = os.listdir(DIR_INPUT)
     for curr_file in files:
-        input.append((curr_file))
+        input.append((DIR_INPUT, curr_file, DIR_OUTPUT, COL_NAMES))
     len_files = len(input)
 
     start_time = timeit.default_timer()
@@ -39,7 +40,8 @@ def main():
     print(f"runtime for converting the data: {end_time - start_time}")
 
 
-def convert_2_hdf5(file):
+def convert_2_hdf5(dir_input, dir_output, file, col_names):
+    KEY = "df"
     filename = os.fsdecode(file)
     if not filename.endswith(".log"): 
         print("ERROR: the file (" + str(filename) + ") should not be part of the directory")
@@ -47,10 +49,10 @@ def convert_2_hdf5(file):
         print("Aborting the program")
         sys.exit()
 
-    path = DIR_INPUT + filename
-    df = pd.read_csv(path, names = COL_NAMES, delim_whitespace = True)
+    path = dir_input + filename
+    df = pd.read_csv(path, names = col_names, delim_whitespace = True)
 
-    df_file_name = DIR_OUTPUT + filename.rsplit('.', 1)[0] + '.h5'
+    df_file_name = dir_output + filename.rsplit('.', 1)[0] + '.h5'
     df.to_hdf(df_file_name, mode = "w", key = KEY)
 
     return
