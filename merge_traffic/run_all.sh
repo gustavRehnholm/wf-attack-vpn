62cbd8c394f402641d4b14f8c0aef133528c1740
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-20 12:19:31 +0200

    Update run_all.sh

--- merge_traffic/run_all.sh
+++ merge_traffic/run_all.sh
@@ -6,8 +6,9 @@
 # to run:
 # ./wf-attack-vpn/merge_traffic/run_all.sh
 
-#python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_50.h5 merged_traffic/twitch_div_rnd_50 0
-#python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_1.h5  merged_traffic/twitch_div_rnd_1  0
+python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_50.h5       merged_traffic/twitch_div_rnd_50      0
+python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_1_first.h5  merged_traffic/twitch_div_rnd_1_first 0
+python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_1_last.h5   merged_traffic/twitch_div_rnd_1_last  0
 
 #python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_50.h5 merged_traffic/twitch_rnd_50 0
 #python wf-attack-vpn/merge_traffic/main.py foreground_traffic background_traffic/twitch_1.h5  merged_traffic/twitch_rnd_1  0
@@ -17,7 +18,8 @@
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_rnd_1  wf-result/twitch_rnd_100/twitch_rnd_2h   100
 
 # divide, randomized order
-python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_div_rnd_50 wf-result/twitch_div_rnd_100/twitch_div_rnd_100h 100
-python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_div_rnd_1  wf-result/twitch_div_rnd_100/twitch_div_rnd_2h   100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_div_rnd_50      wf-result/twitch_div_rnd_100/twitch_div_rnd_100h     100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_div_rnd_1_first wf-result/twitch_div_rnd_100/twitch_div_rnd_first_2h 100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_div_rnd_1_last  wf-result/twitch_div_rnd_100/twitch_div_rnd_last_2h  100
 
 ./wf-attack-vpn/plotter/plot.sh
