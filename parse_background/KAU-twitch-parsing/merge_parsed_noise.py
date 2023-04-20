0e9f80213e5e2bf858b82e0e2f46375709fac0c2
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-20 12:09:48 +0200

    Update merge_parsed_noise.py

--- parse_background/KAU-twitch-parsing/merge_parsed_noise.py
+++ parse_background/KAU-twitch-parsing/merge_parsed_noise.py
@@ -14,7 +14,7 @@ def main():
     DIR_INPUT = "twitch/parsed_captures/"
     # the merged noise file in the h5 format
     DIR_OUTPUT = "background_traffic"
-    FILE_OUTPUT = "twitch_1_last.h5"
+    FILE_OUTPUT = "twitch_50.h5"
     PATH_OUTPUT = DIR_OUTPUT + "/" + FILE_OUTPUT
     COL_NAMES =  ['time', 'direction', 'size']
     # for storing the result as h5
@@ -45,7 +45,7 @@ def main():
 
     df_len = []
 
-    sorted_files.reverse()
+    #sorted_files.reverse()
 
     for file in sorted_files:
         index += 1
