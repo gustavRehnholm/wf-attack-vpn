4e420028456ea26a7a07ba8d4d826a4a6df02b17
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-20 10:26:51 +0200

    refactoring

--- wf-attack/wf-attack-dir.py
+++ wf-attack/wf-attack-dir.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
 
-'''
-wf attack on all files in a directory
-Performed with Deep fingerprinting, on teh modes: default, constant and Tik-Tok
-
-TODO: Stop program if it can not create the result dir
-TODO: check that the input paths exists
-
-python wf-attack-vpn/wf-attack/wf-attack-dir.py
-'''
-
+import argparse
 import os
 import sys
 from multiprocessing import Pool
 import timeit
 
-def main():
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", required=True, default="", help="root folder of the merged dataset")
+ap.add_argument("-r", required=True, default="", help="root folder of the df result")
+ap.add_argument("-s", required=False, type=int, default=100,
+    help="sample to use (100 in total)")
+args = vars(ap.parse_args())
 
-    DIR_MERGED = sys.argv[1]
-    DIR_RESULT = sys.argv[2]
-    SAMPLE     = sys.argv[3]
+def main():
+    '''
+    wf attack on all files in a directory
+    Performed with Deep fingerprinting, on the modes: default, constant and Tik-Tok
+    It have support for multiprocessing, but needs a good computer to make use of it
+    '''
+
+    DIR_MERGED = args['m']
+    DIR_RESULT = args['r']
+    SAMPLE     = args['s']
 
     # create the path for the result
     splitted_merged_path = os.path.dirname(DIR_RESULT)
     os.system("mkdir " + splitted_merged_path)
     os.system("rm -f -r " + DIR_RESULT)
     os.system("mkdir " + DIR_RESULT)
-
     
     input = [
         (DIR_MERGED, SAMPLE, ""          , DIR_RESULT, "default"),
@@ -37,16 +39,6 @@ def main():
     p = Pool(1)
     p.starmap(df_attack, input)
 
-    '''
-    # default wf attack
-    os.system("./df-fitness.py -d " + DIR_MERGED + " --train -s "+ SAMPLE +" --csv " + DIR_RESULT + "/default.csv")
-
-    # constant wf attack
-    os.system("./df-fitness.py -d " + DIR_MERGED + " --train -s "+ SAMPLE +" --constant --csv " + DIR_RESULT + "/constant.csv")
-
-    # tiktok wf attack
-    os.system("./df-fitness.py -d " + DIR_MERGED + " --train -s "+ SAMPLE +" --tiktok --csv " + DIR_RESULT + "/tiktok.csv")
-    '''
     end_time = timeit.default_timer()
     print(f"runtime for this merged dataset: {end_time - start_time}")
     return
--- wf-attack/wf-attack.sh
+++ wf-attack/wf-attack.sh
@@ -3,9 +3,9 @@
 # ./wf-attack-vpn/wf-attack/wf-attack.sh
 
 # non divide, randomized order
-python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_div_rnd_150 wf-result/twitch_rnd_100/twitch_rn_300h 100
-python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_div_rnd_1   wf-result/twitch_rnd_100/twitch_rnd_2h  100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_50 -r wf-result/twitch_rnd_100/twitch_rn_100h
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1   -r wf-result/twitch_rnd_100/twitch_rnd_2h
 
 # divide, randomized order
-python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_div_rnd_150 wf-result/twitch_div_rnd_100/twitch_div_rnd_300h 100
-python wf-attack-vpn/wf-attack/wf-attack-dir.py merged_traffic/twitch_div_rnd_1   wf-result/twitch_div_rnd_100/twitch_div_rnd_2h   100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_50 -r wf-result/twitch_div_rnd_100/twitch_div_rnd_100h
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1   -r wf-result/twitch_div_rnd_100/twitch_div_rnd_2h
