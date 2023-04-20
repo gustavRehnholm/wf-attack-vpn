87a0dc1309f58ace81a0c0ad6fee1dfbbf1ca192
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-20 10:31:30 +0200

    refactoring

--- wf-attack/wf-attack.sh
+++ wf-attack/wf-attack.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# ./wf-attack-vpn/wf-attack/wf-attack.sh
+'''
+Run DF (with all three modes) on the merged datasets
+To run:
+./wf-attack-vpn/wf-attack/wf-attack.sh
+'''
 
-# non divide, randomized order
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_50 -r wf-result/twitch_rnd_100/twitch_rn_100h
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1   -r wf-result/twitch_rnd_100/twitch_rnd_2h
+# wf attack
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_50 -r wf-result/twitch_rnd_100/twitch_rnd_100h -s 100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1  -r wf-result/twitch_rnd_100/twitch_rnd_2h   -s 100
 
 # divide, randomized order
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_50 -r wf-result/twitch_div_rnd_100/twitch_div_rnd_100h
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1   -r wf-result/twitch_div_rnd_100/twitch_div_rnd_2h
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_50 -r wf-result/twitch_div_rnd_100/twitch_div_rnd_100h -s 100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1  -r wf-result/twitch_div_rnd_100/twitch_div_rnd_2h   -s 100
+
