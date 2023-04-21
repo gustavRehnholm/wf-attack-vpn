f29696b42aab724849c64ae906cf3de2331f99e8
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-21 14:35:48 +0200

    special case for large datasets

--- merge_traffic/get_merged.py
+++ merge_traffic/get_merged.py
@@ -80,7 +80,6 @@ def getMerged(dir_foreground, dir_merged, dir_background, fold = 0, workers = 5)
     print("Size of the background")
     print(df_len)
 
-    '''
     if workers == 1:
         # start with the training files, if the program has difficulties in running, it will crash at the training
         print("Start merging train files")
@@ -93,17 +92,16 @@ def getMerged(dir_foreground, dir_merged, dir_background, fold = 0, workers = 5)
         if not mergeTrafficOld(mergedValidFiles, foregroundValidFiles, dir_background, part_of_10 + 1 , part_of_10*2):
             return False
     else:
-    '''
-    # start with the training files, if the program has difficulties in running, it will crash at the training
-    print("Start merging train files")
-    if not mergeTraffic(mergedTrainFiles, foregroundTrainFiles, dir_background, part_of_10*2 + 1, int(df_len), workers):
-        return False
-    print("Start merging test files")
-    if not mergeTraffic(mergedTestFiles , foregroundTestFiles , dir_background, 0              , part_of_10, workers):
-        return False
-    print("Start merging validation files")
-    if not mergeTraffic(mergedValidFiles, foregroundValidFiles, dir_background, part_of_10 + 1 , part_of_10*2, workers):
-        return False
+        # start with the training files, if the program has difficulties in running, it will crash at the training
+        print("Start merging train files")
+        if not mergeTraffic(mergedTrainFiles, foregroundTrainFiles, dir_background, part_of_10*2 + 1, int(df_len), workers):
+            return False
+        print("Start merging test files")
+        if not mergeTraffic(mergedTestFiles , foregroundTestFiles , dir_background, 0              , part_of_10, workers):
+            return False
+        print("Start merging validation files")
+        if not mergeTraffic(mergedValidFiles, foregroundValidFiles, dir_background, part_of_10 + 1 , part_of_10*2, workers):
+            return False
 
     '''
     if not mergeTraffic(mergedTestFiles , foregroundTestFiles , dir_background, 0              , int(df_len)):
