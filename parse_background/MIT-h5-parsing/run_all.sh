abef1974ebff2209d137b570c36996d360ada6fd
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-21 11:01:10 +0200

    refactoring

--- parse_background/MIT-h5-parsing/run_all.sh
+++ parse_background/MIT-h5-parsing/run_all.sh
@@ -1,24 +1,13 @@
 # basic script to run from start to end
 
-
 # Extract VPN traffic, and store them depending on the application
-touch stdout/extract_dataset.txt
-python wf-attack-vpn/Parse_noise/MIT-h5-parsing/extract_dataset.py | tee stdout/extract_dataset.txt
-
+python wf-attack-vpn/Parse_noise/MIT-h5-parsing/extract_dataset.py
 
 # Divide the captures after application useage
-touch stdout/split_dataset_application.txt
-python wf-attack-vpn/Parse_noise/MIT-h5-parsing/split_dataset_application.py | tee stdout/split_dataset_application.txt
-
+python wf-attack-vpn/Parse_noise/MIT-h5-parsing/split_dataset_application.py
 
 # Parse the data (from one capture per row, to one packet per row)
-touch stdout/parse_dataset.txt
-python wf-attack-vpn/Parse_noise/MIT-h5-parsing/parse_dataset.py | tee stdout/parse_dataset.txt
-
+python wf-attack-vpn/Parse_noise/MIT-h5-parsing/parse_dataset.py
 
 # Merge the data after category
-touch stdout/parse_dataset.txt
-python wf-attack-vpn/Parse_noise/MIT-h5-parsing/parse_dataset.py | tee stdout/parse_dataset.txt
-
-
-
+python wf-attack-vpn/Parse_noise/MIT-h5-parsing/parse_dataset.py
