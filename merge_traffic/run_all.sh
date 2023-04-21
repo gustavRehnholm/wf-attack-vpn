f1a7e45812dc0099639af0295c94bcb9f9ab377a
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-21 16:05:11 +0200

    Update run_all.sh

--- merge_traffic/run_all.sh
+++ merge_traffic/run_all.sh
@@ -8,6 +8,8 @@
 
 # merge 
 
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h  -s 100
+
 # divided
 #python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_50.h5       -m merged_traffic/twitch_div_rnd_50       --fold 0 -w 1
 #python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_1_first.h5  -m merged_traffic/twitch_div_rnd_1_first  --fold 0 -w 5
