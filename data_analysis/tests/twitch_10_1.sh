cf8a963020ce4921845df8d847394882a31991c9
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-24 16:19:23 +0200

    Update twitch_10_1.sh

--- data_analysis/tests/twitch_10_1.sh
+++ data_analysis/tests/twitch_10_1.sh
@@ -2,21 +2,21 @@
 # Test to get the start of the foreground traffic, by testing against the capture file with the highest density of packets
 
 # to run:
-# ./wf-attack-vpn/data_analysis/tests/twitch_5_largest.sh
+# ./wf-attack-vpn/data_analysis/tests/twitch_10_1.sh
 
 # merge 
-python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_largest.h5  -m merged_traffic/test/10/twitch_largest_1 --fold 0 -w 5
-python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_largest.h5  -m merged_traffic/test/10/twitch_largest_2 --fold 0 -w 5
-python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_largest.h5  -m merged_traffic/test/10/twitch_largest_3 --fold 0 -w 5
-python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_largest.h5  -m merged_traffic/test/10/twitch_largest_4 --fold 0 -w 5
-python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_largest.h5  -m merged_traffic/test/10/twitch_largest_5 --fold 0 -w 5
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_largest.h5  -m merged_traffic/test/10_1/twitch_largest_1 --fold 0 -w 5
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_largest.h5  -m merged_traffic/test/10_1/twitch_largest_2 --fold 0 -w 5
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_largest.h5  -m merged_traffic/test/10_1/twitch_largest_3 --fold 0 -w 5
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_largest.h5  -m merged_traffic/test/10_1/twitch_largest_4 --fold 0 -w 5
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_largest.h5  -m merged_traffic/test/10_1/twitch_largest_5 --fold 0 -w 5
 
 # analysis
-python wf-attack-vpn/data_analysis/foreground_cleaning.py -d merged_traffic/test/10/twitch_largest_1
-python wf-attack-vpn/data_analysis/foreground_cleaning.py -d merged_traffic/test/10/twitch_largest_2
-python wf-attack-vpn/data_analysis/foreground_cleaning.py -d merged_traffic/test/10/twitch_largest_3
-python wf-attack-vpn/data_analysis/foreground_cleaning.py -d merged_traffic/test/10/twitch_largest_4
-python wf-attack-vpn/data_analysis/foreground_cleaning.py -d merged_traffic/test/10/twitch_largest_5
+python wf-attack-vpn/data_analysis/foreground_cleaning.py -d merged_traffic/test/10_1/twitch_largest_1
+python wf-attack-vpn/data_analysis/foreground_cleaning.py -d merged_traffic/test/10_1/twitch_largest_2
+python wf-attack-vpn/data_analysis/foreground_cleaning.py -d merged_traffic/test/10_1/twitch_largest_3
+python wf-attack-vpn/data_analysis/foreground_cleaning.py -d merged_traffic/test/10_1/twitch_largest_4
+python wf-attack-vpn/data_analysis/foreground_cleaning.py -d merged_traffic/test/10_1/twitch_largest_5
 
 # WF attack
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/test/5/twitch_largest_1 -r wf-result/test/5/twitch_largest_1 -s 100
