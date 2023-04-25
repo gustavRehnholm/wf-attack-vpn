e48eec749150c597554c357d01edf0c7003feca6
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-25 12:19:32 +0200

    Update twitch.sh

--- data_analysis/tests/twitch.sh
+++ data_analysis/tests/twitch.sh
@@ -5,17 +5,17 @@
 # ./wf-attack-vpn/data_analysis/tests/twitch.sh
 
 # merge 
-#python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_40.h5 -m merged_traffic/test/40/twitch_10_10 --ffold 0 -w 1
-#python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_40.h5 -m merged_traffic/test/40/twitch_0     --ffold 0 -w 1
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_40.h5 -m merged_traffic/test/40/twitch_10_10_1 --ffold 0 -w 1
+#python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_40.h5 -m merged_traffic/test/40/twitch_0_1     --ffold 0 -w 1
 
 # analysis
-#python wf-attack-vpn/data_analysis/merged-foreground-ratio.py -d merged_traffic/test/40/twitch_10_10
+python wf-attack-vpn/data_analysis/merged-foreground-ratio.py -d merged_traffic/test/40/twitch_10_10_1
 #python wf-attack-vpn/data_analysis/merged-foreground-ratio.py -d merged_traffic/test/40/twitch_0
 
 # wf attack 
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/test/40/twitch_0     -r wf-result/twitch_40/twitch_0  -s 100
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/test/40/twitch_10_10 -r wf-result/twitch_40/twitch_10 -s 100
+#python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/test/40/twitch_0_1     -r wf-result/twitch_40/twitch_0_1  -s 100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/test/40/twitch_10_10_1 -r wf-result/twitch_40/twitch_10_1 -s 100
 
 # plot
-python wf-attack-vpn/plotter/plot_all.py --title twitch_0     --input wf-result/twitch_40/twitch_0     --output fig/twitch_40
-python wf-attack-vpn/plotter/plot_all.py --title twitch_10_10 --input wf-result/twitch_40/twitch_10    --output fig/twitch_40
+#python wf-attack-vpn/plotter/plot_all.py --title twitch_0_1     --input wf-result/twitch_40/twitch_0_1     --output fig/twitch_40
+python wf-attack-vpn/plotter/plot_all.py --title twitch_10_1 --input wf-result/twitch_40/twitch_10_1    --output fig/twitch_40
