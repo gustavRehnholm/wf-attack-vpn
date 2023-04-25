2e73e180193781bfece95a07df1879cabd3edc44
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-25 08:47:52 +0200

    Update twitch.sh

--- data_analysis/tests/twitch.sh
+++ data_analysis/tests/twitch.sh
@@ -5,7 +5,16 @@
 # ./wf-attack-vpn/data_analysis/tests/twitch_5_10.sh
 
 # merge 
-python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_10.h5 -m merged_traffic/test/10/twitch_0 --ffold 0 -w 5
+#python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/test/twitch_10.h5 -m merged_traffic/test/10/twitch_10_10 --ffold 0 -w 5
 
 # analysis
-python wf-attack-vpn/data_analysis/merged-foreground-ratio.py -d merged_traffic/test/10/twitch_0
+#python wf-attack-vpn/data_analysis/merged-foreground-ratio.py -d merged_traffic/test/10/twitch_10_10
+
+# wf attack 
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/test/10/twitch_0     -r wf-result/twitch_10/twitch_0  -s 100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/test/10/twitch_5_10  -r wf-result/twitch_10/twitch_5  -s 100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/test/10/twitch_10_10 -r wf-result/twitch_10/twitch_10 -s 100
+
+python wf-attack-vpn/plotter/plot_all.py --title twitch_0     --input wf-result/twitch_10/twitch_0     --output fig/twitch_10
+python wf-attack-vpn/plotter/plot_all.py --title twitch_5_10  --input wf-result/twitch_10/twitch_5_10  --output fig/twitch_10
+python wf-attack-vpn/plotter/plot_all.py --title twitch_10_10 --input wf-result/twitch_10/twitch_10_10 --output fig/twitch_10
