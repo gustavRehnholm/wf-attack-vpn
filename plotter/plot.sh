b264428404e18052df5842585843466f82f6345a
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-26 15:10:43 +0200

    updated for the new implementation of wf attacks

--- plotter/plot.sh
+++ plotter/plot.sh
@@ -10,37 +10,42 @@ python wf-attack-vpn/plotter/plot_all.py --title twitch_old_og  --input wf-resul
 python wf-attack-vpn/plotter/plot_all.py --title twitch_old_20  --input wf-result/twitch_old/old_twitch_20  --output fig/twitch_old
 python wf-attack-vpn/plotter/plot_all.py --title twitch_old_100 --input wf-result/twitch_old/old_twitch_100 --output fig/twitch_old
 
-# rnd, sample 100
+# non-div rnd
 python wf-attack-vpn/plotter/plot_all.py --title twitch_rnd_80h       --input wf-result/twitch_rnd/twitch_rnd_80h       --output fig/twitch_rnd
 python wf-attack-vpn/plotter/plot_all.py --title twitch_rnd_first_2h  --input wf-result/twitch_rnd/twitch_rnd_first_2h  --output fig/twitch_rnd
 python wf-attack-vpn/plotter/plot_all.py --title twitch_rnd_last_2h   --input wf-result/twitch_rnd/twitch_rnd_last_2h   --output fig/twitch_rnd
 python wf-attack-vpn/plotter/plot_all.py --title twitch_rnd_middle_2h --input wf-result/twitch_rnd/twitch_rnd_middle_2h --output fig/twitch_rnd
 
-# Divided, randomized, sample 100 (the largest part of the Twitch dataset)
+# div-rnd
 python wf-attack-vpn/plotter/plot_all.py --title twitch_div_rnd_80h       --input wf-result/twitch_div_rnd/twitch_div_rnd_80h       --output fig/twitch_div_rnd
 python wf-attack-vpn/plotter/plot_all.py --title twitch_div_rnd_first_2h  --input wf-result/twitch_div_rnd/twitch_div_rnd_first_2h  --output fig/twitch_div_rnd
 python wf-attack-vpn/plotter/plot_all.py --title twitch_div_rnd_last_2h   --input wf-result/twitch_div_rnd/twitch_div_rnd_last_2h   --output fig/twitch_div_rnd
 python wf-attack-vpn/plotter/plot_all.py --title twitch_div_rnd_middle_2h --input wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h --output fig/twitch_div_rnd
 
+# div-rnd higher epoch
+python wf-attack-vpn/plotter/plot_all.py --title twitch_div_rnd_middle_2h_epoch_60 --input wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h_epoch_60 --output fig/twitch_div_rnd_epoch
+python wf-attack-vpn/plotter/plot_all.py --title twitch_div_rnd_middle_2h_epoch_300 --input wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h_epoch_300 --output fig/twitch_div_rnd_epoch
+python wf-attack-vpn/plotter/plot_all.py --title twitch_div_rnd_middle_2h_epoch_1000 --input wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h_epoch_1000 --output fig/twitch_div_rnd_epoch
+
 # cp files for the comparison
 
 # 100 default
-cp wf-result/undef_100/combined/combined.csv                    wf-result/compare_default/undef.csv
-cp wf-result/twitch_old/old_twitch_100/default.csv              wf-result/compare_default/twitch.csv
-cp wf-result/twitch_div_rnd/twitch_div_rnd_last_2h/default.csv  wf-result/compare_default/rnd_last.csv
-cp wf-result/twitch_div_rnd/twitch_div_rnd_first_2h/default.csv wf-result/compare_default/rnd_first.csv
+cp wf-result/undef_100/combined/combined.csv                     wf-result/compare_default/undef.csv
+cp wf-result/twitch_old/old_twitch_100/default.csv               wf-result/compare_default/twitch.csv
+cp wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h/default.csv wf-result/compare_default/div_rnd.csv
+cp wf-result/twitch_div_rnd/twitch_rnd_middle_2h/default.csv     wf-result/compare_default/rnd.csv
 
 # 100 constant
-cp wf-result/undef_100/combined/combined.csv                     wf-result/compare_constant/undef.csv
-cp wf-result/twitch_old/old_twitch_100/constant.csv              wf-result/compare_constant/twitch.csv
-cp wf-result/twitch_div_rnd/twitch_div_rnd_last_2h/constant.csv  wf-result/compare_constant/rnd_last.csv
-cp wf-result/twitch_div_rnd/twitch_div_rnd_first_2h/constant.csv wf-result/compare_constant/rnd_first.csv
+cp wf-result/undef_100/combined/combined.csv                      wf-result/compare_constant/undef.csv
+cp wf-result/twitch_old/old_twitch_100/constant.csv               wf-result/compare_constant/twitch.csv
+cp wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h/constant.csv wf-result/compare_constant/div_rnd.csv
+cp wf-result/twitch_div_rnd/twitch_rnd_middle_2h/constant.csv     wf-result/compare_constant/rnd.csv
 
 # 100 Tik-Tok
-cp wf-result/undef_100/combined/combined.csv                   wf-result/compare_tiktok/undef.csv
-cp wf-result/twitch_old/old_twitch_100/tiktok.csv              wf-result/compare_tiktok/twitch.csv
-cp wf-result/twitch_div_rnd/twitch_div_rnd_last_2h/tiktok.csv  wf-result/compare_tiktok/rnd_last.csv
-cp wf-result/twitch_div_rnd/twitch_div_rnd_first_2h/tiktok.csv wf-result/compare_tiktok/rnd_first.csv
+cp wf-result/undef_100/combined/combined.csv                    wf-result/compare_tiktok/undef.csv
+cp wf-result/twitch_old/old_twitch_100/tiktok.csv               wf-result/compare_tiktok/twitch.csv
+cp wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h/tiktok.csv wf-result/compare_tiktok/div_rnd.csv
+cp wf-result/twitch_div_rnd/twitch_rnd_middle_2h/tiktok.csv     wf-result/compare_tiktok/rnd.csv
 
 # The final comparison graph
 python wf-attack-vpn/plotter/plot_all.py --title compare_default  --input wf-result/compare_default  --output fig/compare
--- wf-attack/wf-attack.sh
+++ wf-attack/wf-attack.sh
@@ -4,8 +4,19 @@
 Run DF (with all three modes) on the merged datasets
 '''
 
+# non-divided
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_40       -r wf-result/twitch_rnd/twitch_rnd_80h       -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_first  -r wf-result/twitch_rnd/twitch_rnd_first_2h  -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_last   -r wf-result/twitch_rnd/twitch_rnd_last_2h   -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf-result/twitch_rnd/twitch_rnd_middle_2h -s 100 --epochs 30
+
 # divide, randomized order
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_50       -r wf-result/twitch_div_rnd/twitch_div_rnd_100h       -s 100
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_first  -r wf-result/twitch_div_rnd/twitch_div_rnd_first_2h   -s 100
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_last   -r wf-result/twitch_div_rnd/twitch_div_rnd_last_2h    -s 100
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h  -s 100
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_50       -r wf-result/twitch_div_rnd/twitch_div_rnd_100h       -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_first  -r wf-result/twitch_div_rnd/twitch_div_rnd_first_2h   -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_last   -r wf-result/twitch_div_rnd/twitch_div_rnd_last_2h    -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h  -s 100 --epochs 30
+
+# higher epochs
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h_epoch_60   -s 100 --epochs 60
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h_epoch_300  -s 100 --epochs 300
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h_epoch_1000 -s 100 --epochs 1000
