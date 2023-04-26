7cc20b9d25277b40c7c6a0d8d3fbb30cc5c20826
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-26 23:24:30 +0200

    Update run_all.sh

--- merge_traffic/run_all.sh
+++ merge_traffic/run_all.sh
@@ -28,6 +28,11 @@
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_last   -r wf-result/twitch_rnd/twitch_rnd_last_2h   -s 100 --epochs 30
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf-result/twitch_rnd/twitch_rnd_middle_2h -s 100 --epochs 30
 
+# divided, with higher epoch
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf-result/twitch_rnd/twitch_div_rnd_middle_2h_epoch_60   -s 100 --epochs 60
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf-result/twitch_rnd/twitch_div_rnd_middle_2h_epoch_300  -s 100 --epochs 300
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf-result/twitch_rnd/twitch_div_rnd_middle_2h_epoch_1000 -s 100 --epochs 1000
+
 # divided
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_40       -r wf-result/twitch_div_rnd/twitch_div_rnd_80h       -s 100 --epochs 30
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_first  -r wf-result/twitch_div_rnd/twitch_div_rnd_first_2h  -s 100 --epochs 30
@@ -37,6 +42,6 @@
 # divided, with higher epoch
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h_epoch_60   -s 100 --epochs 60
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h_epoch_300  -s 100 --epochs 300
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h_epoch_1000 -s 100 --epochs 1000
+#python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf-result/twitch_div_rnd/twitch_div_rnd_middle_2h_epoch_1000 -s 100 --epochs 1000
 
 ./wf-attack-vpn/plotter/plot.sh
