c8522121d738c782aca8d7d3bfdf3016e4889982
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-30 09:25:10 +0200

    Create background_graph.py

---
+++ data_analysis/background_graph.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python3
+
+'''
+To run
+python wf-attack-vpn/parse_background/KAU-twitch-parsing/analyze_dataset.py --input twitch/tmp --workers 1
+'''
+
+from multiprocessing import Pool
+import pandas            as pd
+import seaborn           as sns
+import numpy             as np
+import matplotlib.pyplot as plt
+import os
+import sys
+import argparse
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("--input"  , required = False, default = "captures_clean/"           , type = str, help = "Directory for the files to analyze")
+ap.add_argument("--output" , required = False, default = "fig/twitch_analysis/"     , type = str, help = "Directory to store the result")
+ap.add_argument("--workers", required = False, default = 10                         , type = int, help = "Workers for multiprocessing")
+args = vars(ap.parse_args())
+
+
+def main():
+    '''
+    Analyze the provided background traffic
+    '''
+    print("Start analyzing background")
+    background_graph(dir_input   = args['input'],
+                    dir_output  = args['output'],
+                    workers     = args['workers'])
+    return
+
+
+def background_graph(dir_input = "captures_clean/", dir_output = "fig/twitch_analysis", workers = 10):
+    '''
+    Analyze the parsed Twitch dataset
+
+    Args:
+        dir_input  - Optional : Path to the captures to analyze (str)
+        dir_output - Optional : Path to the twitch analysis     (str)
+        workers    - Optional : Workers for multiprocessing     (int)
+    '''
+
+    # usable captures, sorted after size (descending)
+    input_files  = os.listdir(dir_input)
+    sorted_files = sorted(input_files, key =  lambda x: os.stat(os.path.join(dir_input, x)).st_size)
+    sorted_files = list(reversed(sorted_files))
+
+    input = []
+    index = 0
+    for curr_file in sorted_files:
+        path = dir_input + "/" + os.fsdecode(curr_file)
+        input.append((path, index))
+        index += 1
+
+    p = Pool(workers)
+
+    # list of pkt/s for each second interval
+    print("Start extracting pkt/sec for each sec interval")
+    time_lists = p.starmap(timestamps_capture, input)
+
+    # list of min, max and mean pkt/s for each captures
+    print("Start extracting min, max and mean for each capture file")
+    stat_lists = p.starmap(stat, time_lists)
+
+    print("Plot the data")
+    # sort the captures after the original order (size descending)
+    sorted_stats = sorted(stat_lists     , key = lambda d: d['index'])
+
+    min  = []
+    max  = []
+    mean = []
+    upper_limit_h = []
+    prev_index = -1
+    for dic in sorted_stats:
+        if prev_index < dic['index']:
+            prev_index = dic['index']
+        else:
+            print(f"ERROR, packets out of order!")
+            sys.exit()
+        min.append(dic['stat'][0])
+        max.append(dic['stat'][1])
+        mean.append(dic['stat'][2])
+        print(dic['stat'])
+        upper_limit_h.append(dic['upper_limit_h'])
+
+    upper_limit_h.sort()
+    print(f"time difference [{upper_limit_h[0]:.2f},{upper_limit_h[-1]:.2f}]")
+
+    # plot a line for min, max and mean
+    plot_analysis(min, max, mean, "Twitch_analysis", "fig/")
+
+    # mean test
+    time_lists_mean = p.starmap(timestamps_capture_mean, input)
+    sorted_means = sorted(time_lists_mean, key = lambda d: d['index'])
+    mean_tmp = []
+    for dic in sorted_means:
+        mean_tmp.append(dic['mean'])
+        print(dic['mean'])
+    
+    plot_analysis(min, max, mean_tmp, "Twitch_analysis_tmp", "fig/")
+
+    return
+
+
+def timestamps_capture(path_file2analyze, index):
+    '''
+    get list of number of packet, each second, for the file
+
+    Args:
+        path_file2analyze - Required : path to the file                       (str)
+    '''
+    time_list = [0]
+    # keep track of current packet and time interval
+    interval_index   = 0
+    lower_limit = interval_index 
+    upper_limit = interval_index + 1
+
+    with open(path_file2analyze, 'r') as file2analyze:
+        for file_line in file2analyze: #Reading line by line from the master file since it might be to large to do readlines() on
+            split_line = file_line.split("\t")
+            time_stamp = split_line[0].split('.')
+            added_line = False
+            while added_line == False:
+                # this packet is in the current interval
+                if time_stamp >= lower_limit and time_stamp < upper_limit:
+                    time_list[interval_index] += 1
+                    added_line                 = True
+                # advance the interval
+                elif curr_timestamp >= upper_limit:
+                    interval_index += 1
+                    time_list.append(0)
+                    # advance the time with the duration of the next packet
+                    lower_limit = interval_index
+                    upper_limit = interval_index + 1
+                # the packet is probably in the wrong order
+                else:
+                    print(f"ERROR: the time {curr_timestamp} should not be able to go below the current lower interval {lower_limit}")
+                    print(f"Upper limit: {upper_limit}")
+
+    return (time_list, index, upper_limit)
+
+
+
+def stat(timestamp_list, index, upper_limit):
+    np_timestamp = np.array(timestamp_list)
+
+    min  = np.min(np_timestamp)
+    max  = np.max(np_timestamp)
+    mean = np.mean(np_timestamp)
+
+    upper_limit_h = upper_limit/(1000000000 * 60 * 60)
+
+    return {"stat": (min, max, mean),
+            "index": index,
+            "upper_limit_h" : upper_limit_h}
+
+
+def plot_analysis(min, max, mean, title = "untitled", result_path = "fig/"):
+    '''
+
+    '''
+
+    plt.plot(mean, label = 'mean')
+    #plt.plot(max, label = 'max')
+    #plt.plot(min)
+
+    plt.ylabel('pkt/s')
+    plt.xlabel('file')
+
+    # Tweak spacing to prevent clipping of tick-labels
+    plt.subplots_adjust(bottom = 0.15)
+
+    plt.legend()
+    plt.title(title)
+    #fig = plt.gcf()
+    file_path = result_path + title + ".png"
+    f = open(file_path, "w")
+    f.close()
+    plt.savefig(file_path)
+
+    #plt.show()
+
+    return
+
+if __name__=="__main__":
+    main()
