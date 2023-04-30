b103fbefd2c3f2b689534e2479be0b458f846638
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-04-30 16:41:31 +0200

    Update plot_figure.py

--- plot_graphs/plot_figure.py
+++ plot_graphs/plot_figure.py
@@ -1,10 +1,10 @@
-
+import sys
 
 def plot_figure(files2plot, subtitle ,x_label, y_label, sup_title = "", result_path  = "fig/" ):
     '''
     Plot a figure of the provided subplots (3 or 4)
     Args:
-        files       - Required : files to plot                    (List[str])
+        files       - Required : path to files to plot                    (List[str])
         sub_title   - Required : list fo titles for each subgraph (List[str])
         x_label     - Required : label for the x axis             (str)
         y_label     - Required : label for the y axis             (str)
@@ -12,12 +12,18 @@ def plot_figure(files2plot, subtitle ,x_label, y_label, sup_title = "", result_p
         result_path - Optional : where to store the figure        (str)
     '''
 
-    if len(files2plot) == 4:
+    # Extract all csv files that should be plotted in a graph
+    datasets = []
+    for csv_file in files2plot:
+        df = pd.read_csv(csv_file, usecols = ["th", "accuracy"], index_col = None)
+        datasets.append(df)
+
+    if len(datasets) == 4:
         fig, axes = plt.subplots(2, 2, figsize=(10, 10))
         fig.subplots_adjust(top=0.8)
 
         for i in range(4):
-            axes[(i%2),(i%1)].plot(files2plot[i])
+            axes[(i%2),(i%1)].plot(datasets[i])
             axes[(i%2),(i%1)].set_title(subtitle[i])
             axes[(i%2),(i%1)].set_ylabel(y_label)
             axes[(i%2),(i%1)].set_xlabel(x_label)
@@ -28,12 +34,12 @@ def plot_figure(files2plot, subtitle ,x_label, y_label, sup_title = "", result_p
         fig.savefig(f"{result_path}{title}.png")
         plt.close(fig)
 
-    elif len(files2plot) == 3:
+    elif len(datasets) == 3:
         fig, axes = plt.subplots(3, 1, figsize=(10, 10))
         fig.subplots_adjust(top=0.8)
 
         for i in range(3):
-            axes[i].plot(files2plot[i])
+            axes[i].plot(datasets[i])
             axes[i].set_title(subtitle[i])
             axes[i].set_ylabel(y_label)
             axes[i].set_xlabel(x_label)
@@ -43,5 +49,9 @@ def plot_figure(files2plot, subtitle ,x_label, y_label, sup_title = "", result_p
         fig.tight_layout()
         fig.savefig(f"{result_path}{title}.png")
         plt.close(fig)
+
+    else:
+        print("ERROR: One must use 3 or 4 files per figure")
+        sys.exit()
 
     return
