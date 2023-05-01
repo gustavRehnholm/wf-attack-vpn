1158e8bf55a60d4872221ad3494ef55c61d115f6
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-01 11:57:44 +0200

    Update plot_figure.py

--- plot_graphs/plot_figure.py
+++ plot_graphs/plot_figure.py
@@ -28,6 +28,8 @@ def plot_figure(figure_dir ,x_label, y_label, sup_title = "", result_path  = "fi
         path = f"{figure_dir}/{subplot_dir}"
         datasets = []
         for csv_file in path:
+            print(csv_file)
+            print(path)
             df = pd.read_csv(csv_file, usecols = ["th", "accuracy"], index_col = None)
             datasets.append(df)
             subtitle[subplot_index].append(csv_file.split('/')[-1])
