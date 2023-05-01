ca356f763cca38161e904896a0cbcb601e679bdb
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-01 17:38:23 +0200

    Update plot.sh

--- plot_graphs/plot.sh
+++ plot_graphs/plot.sh
@@ -6,13 +6,13 @@
 # cp files for the comparison
 
 # 100 default
-cp wf_result/undef_100/combined/combined.csv                     wf_result/compare/compare_default/undef.csv
+#cp wf_result/undef_100/combined/combined.csv                     wf_result/compare/compare_default/undef.csv
 cp wf_result/twitch_old/old_twitch_100/default.csv               wf_result/compare/compare_default/twitch.csv
 cp wf_result/twitch_div_rnd/twitch_div_rnd_middle_2h/default.csv wf_result/compare/compare_default/div_rnd.csv
 cp wf_result/twitch_rnd/twitch_rnd_middle_2h/default.csv         wf_result/compare/compare_default/rnd.csv
 
 # 100 constant
-cp wf_result/undef_100/combined/combined.csv                      wf_result/compare/compare_constant/undef.csv
+#cp wf_result/undef_100/combined/combined.csv                      wf_result/compare/compare_constant/undef.csv
 cp wf_result/twitch_old/old_twitch_100/constant.csv               wf_result/compare/compare_constant/twitch.csv
 cp wf_result/twitch_div_rnd/twitch_div_rnd_middle_2h/constant.csv wf_result/compare/compare_constant/div_rnd.csv
 cp wf_result/twitch_rnd/twitch_rnd_middle_2h/constant.csv         wf_result/compare/compare_constant/rnd.csv
