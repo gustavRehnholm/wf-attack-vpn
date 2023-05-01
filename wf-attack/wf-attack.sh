b85cdf23f58bc6023dceaf72bb1697392c6588b5
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-01 18:09:29 +0200

    Update wf-attack.sh

--- wf-attack/wf-attack.sh
+++ wf-attack/wf-attack.sh
@@ -2,6 +2,7 @@
 
 '''
 Run DF (with all three modes) on the merged datasets
+python wf-attack-vpn/wf-attack/wf-attack.sh
 '''
 
 # non-divided
@@ -25,3 +26,5 @@ python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd
 python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf_result/twitch_rnd_epoch/twitch_rnd_middle_2h_epoch_60   -s 100 --epochs 60
 python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf_result/twitch_rnd_epoch/twitch_rnd_middle_2h_epoch_300  -s 100 --epochs 300
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf_result/twitch_rnd_epoch/twitch_rnd_middle_2h_epoch_1000 -s 100 --epochs 1000
+
+./wf-attack-vpn/plot_graphs/plot.sh
