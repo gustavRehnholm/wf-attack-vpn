f3a686f001d51bf533fa4c1b460830e8f4167c71
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-02 11:24:03 +0200

    test run

--- wf-attack/wf-attack.sh
+++ wf-attack/wf-attack.sh
@@ -7,15 +7,15 @@ Run DF (with all three modes) on the merged datasets
 
 # non-divided
 python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_40       -r wf_result/twitch_rnd/twitch_rnd_80h       -s 100 --epochs 30
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_first  -r wf_result/twitch_rnd/twitch_rnd_first_2h  -s 100 --epochs 30
+#python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_first  -r wf_result/twitch_rnd/twitch_rnd_first_2h  -s 100 --epochs 30
 python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_last   -r wf_result/twitch_rnd/twitch_rnd_last_2h   -s 100 --epochs 30
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf_result/twitch_rnd/twitch_rnd_middle_2h -s 100 --epochs 30
+#python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf_result/twitch_rnd/twitch_rnd_middle_2h -s 100 --epochs 30
 
 # divide, randomized order
 python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_40       -r wf_result/twitch_div_rnd/twitch_div_rnd_80h        -s 100 --epochs 30
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_first  -r wf_result/twitch_div_rnd/twitch_div_rnd_first_2h   -s 100 --epochs 30
+#python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_first  -r wf_result/twitch_div_rnd/twitch_div_rnd_first_2h   -s 100 --epochs 30
 python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_last   -r wf_result/twitch_div_rnd/twitch_div_rnd_last_2h    -s 100 --epochs 30
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf_result/twitch_div_rnd/twitch_div_rnd_middle_2h  -s 100 --epochs 30
+#python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf_result/twitch_div_rnd/twitch_div_rnd_middle_2h  -s 100 --epochs 30
 
 # higher epochs, divide
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf_result/twitch_div_rnd_epoch/twitch_div_rnd_middle_2h_epoch_60   -s 100 --epochs 60
