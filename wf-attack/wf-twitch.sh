e36e1e8e4bf3e83e880dcc270b05b7774521cd34
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-03 20:38:40 +0200

    Update wf-twitch.sh

--- wf-attack/wf-twitch.sh
+++ wf-attack/wf-twitch.sh
@@ -19,11 +19,13 @@ Run DF (with all three modes) on the merged datasets with Twitch
 
 
 # higher epochs, div-rnd
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf_result/twitch_div_rnd_epoch/_2h_epoch_60   -s 100 --epochs 60
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf_result/twitch_div_rnd_epoch/_2h_epoch_300  -s 100 --epochs 300
+#python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf_result/twitch_div_rnd_epoch/_2h_epoch_60   -s 100 --epochs 60
+#python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf_result/twitch_div_rnd_epoch/_2h_epoch_300  -s 100 --epochs 300
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_div_rnd_1_middle -r wf_result/twitch_div_rnd_epoch/_2h_epoch_1000  -s 100 --epochs 1000
 
 # higher epochs, rnd
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf_result/twitch_rnd_epoch/_2h_epoch_60   -s 100 --epochs 60
-python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf_result/twitch_rnd_epoch/_2h_epoch_300  -s 100 --epochs 300
+#python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf_result/twitch_rnd_epoch/_2h_epoch_60   -s 100 --epochs 60
+#python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf_result/twitch_rnd_epoch/_2h_epoch_300  -s 100 --epochs 300
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/twitch_rnd_1_middle -r wf_result/twitch_rnd_epoch/_2h_epoch_1000  -s 100 --epochs 1000
 
 ./wf-attack-vpn/plot_graphs/plot.sh
