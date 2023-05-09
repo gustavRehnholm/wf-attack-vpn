b05ad237c41ac0f9582de85274177357ba6ba31c
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-09 22:40:25 +0200

    cleanup

--- merge_traffic/run_all_mit.sh
+++ merge_traffic/run_all_mit.sh
@@ -4,49 +4,12 @@
 # to run:
 # ./wf-attack-vpn/merge_traffic/run_all_mit.sh
 
-# merge 
-
-# 10-fold voip
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/voip.h5 -m merged_traffic/voip_0 --bfold 0 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/voip.h5 -m merged_traffic/voip_1 --bfold 1 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/voip.h5 -m merged_traffic/voip_2 --bfold 2 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/voip.h5 -m merged_traffic/voip_3 --bfold 3 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/voip.h5 -m merged_traffic/voip_4 --bfold 4 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/voip.h5 -m merged_traffic/voip_5 --bfold 5 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/voip.h5 -m merged_traffic/voip_6 --bfold 6 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/voip.h5 -m merged_traffic/voip_7 --bfold 7 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/voip.h5 -m merged_traffic/voip_8 --bfold 8 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/voip.h5 -m merged_traffic/voip_9 --bfold 9 -w 10
-
-
-# 10-fold rdp
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/rdp.h5 -m merged_traffic/mit/rdp/fold_0 --bfold 0 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/rdp.h5 -m merged_traffic/mit/rdp/fold_1 --bfold 1 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/rdp.h5 -m merged_traffic/mit/rdp/fold_2 --bfold 2 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/rdp.h5 -m merged_traffic/mit/rdp/fold_3 --bfold 3 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/rdp.h5 -m merged_traffic/mit/rdp/fold_4 --bfold 4 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/rdp.h5 -m merged_traffic/mit/rdp/fold_5 --bfold 5 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/rdp.h5 -m merged_traffic/mit/rdp/fold_6 --bfold 6 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/rdp.h5 -m merged_traffic/mit/rdp/fold_7 --bfold 7 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/rdp.h5 -m merged_traffic/mit/rdp/fold_8 --bfold 8 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/rdp.h5 -m merged_traffic/mit/rdp/fold_9 --bfold 9 -w 10
-
-# 10-fold vimeo
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_0 --bfold 0 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_1 --bfold 1 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_2 --bfold 2 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_3 --bfold 3 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_4 --bfold 4 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_5 --bfold 5 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_6 --bfold 6 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_7 --bfold 7 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_8 --bfold 8 -w 10
-#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_9 --bfold 9 -w 10
+# create 10-fold merged datastes of the applications from the MIT dataset
 
 python wf-attack-vpn/merge_traffic/run_mit.py -app netflix
-#python wf-attack-vpn/merge_traffic/run_mit.py -app rdp
-python wf-attack-vpn/merge_traffic/run_mit.py -app skype
+python wf-attack-vpn/merge_traffic/run_mit.py -app rdp
+python wf-attack-vpn/merge_traffic/run_mit.py -app skype-chat
 python wf-attack-vpn/merge_traffic/run_mit.py -app ssh
-#python wf-attack-vpn/merge_traffic/run_mit.py -app vimeo
-#python wf-attack-vpn/merge_traffic/run_mit.py -app voip
+python wf-attack-vpn/merge_traffic/run_mit.py -app vimeo
+python wf-attack-vpn/merge_traffic/run_mit.py -app voip
 python wf-attack-vpn/merge_traffic/run_mit.py -app youtube
--- wf-attack/run_all_mit.sh
+++ wf-attack/run_all_mit.sh
@@ -11,6 +11,6 @@ Run DF (with all three modes) on the merged datasets with MIT
 #python wf-attack-vpn/wf-attack/wf_mit.py --app netflix
 python wf-attack-vpn/wf-attack/wf_mit.py --app skype-chat
 python wf-attack-vpn/wf-attack/wf_mit.py --app ssh
-python wf-attack-vpn/wf-attack/wf_mit.py --app youtube
+#python wf-attack-vpn/wf-attack/wf_mit.py --app youtube
 
 python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/mit/ -g fig/mit/ -w 10
