62306352138fd2938bcd8c8c9a2025f47304bcf0
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-09 08:09:10 +0200

    Update wf-mit.sh

--- wf-attack/wf-mit.sh
+++ wf-attack/wf-mit.sh
@@ -5,7 +5,7 @@ Run DF (with all three modes) on the merged datasets with MIT
 ./wf-attack-vpn/wf-attack/wf-mit.sh
 '''
 
-# 1-fold voip
+# 10-fold voip
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/voip/voip_0 -r wf_result/mit/voip/fold_0 -s 100 --epochs 30
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/voip/voip_1 -r wf_result/mit/voip/fold_1 -s 100 --epochs 30
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/voip/voip_2 -r wf_result/mit/voip/fold_2 -s 100 --epochs 30
@@ -16,5 +16,17 @@ Run DF (with all three modes) on the merged datasets with MIT
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/voip/voip_7 -r wf_result/mit/voip/fold_7 -s 100 --epochs 30
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/voip/voip_8 -r wf_result/mit/voip/fold_8 -s 100 --epochs 30
 #python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/voip/voip_9 -r wf_result/mit/voip/fold_9 -s 100 --epochs 30
+
+# 10-fold voip
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/rdp/fold_0 -r wf_result/mit/rdp/fold_0 -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/rdp/fold_1 -r wf_result/mit/rdp/fold_1 -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/rdp/fold_2 -r wf_result/mit/rdp/fold_2 -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/rdp/fold_3 -r wf_result/mit/rdp/fold_3 -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/rdp/fold_4 -r wf_result/mit/rdp/fold_4 -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/rdp/fold_5 -r wf_result/mit/rdp/fold_5 -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/rdp/fold_6 -r wf_result/mit/rdp/fold_6 -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/rdp/fold_7 -r wf_result/mit/rdp/fold_7 -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/rdp/fold_8 -r wf_result/mit/rdp/fold_8 -s 100 --epochs 30
+python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/rdp/fold_9 -r wf_result/mit/rdp/fold_9 -s 100 --epochs 30
 
 python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/mit/ -g fig/mit/ -w 10
