72f22ee5bbe4cd7cbc639759b5fbef4507796bd3
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-09 17:50:10 +0200

    cleaner way of creating merged datasets from MIT

--- merge_traffic/run_all_mit.sh
+++ merge_traffic/run_all_mit.sh
@@ -32,13 +32,21 @@
 #python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/rdp.h5 -m merged_traffic/mit/rdp/fold_9 --bfold 9 -w 10
 
 # 10-fold vimeo
-python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_0 --bfold 0 -w 10
-python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_1 --bfold 1 -w 10
-python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_2 --bfold 2 -w 10
-python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_3 --bfold 3 -w 10
-python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_4 --bfold 4 -w 10
-python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_5 --bfold 5 -w 10
-python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_6 --bfold 6 -w 10
-python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_7 --bfold 7 -w 10
-python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_8 --bfold 8 -w 10
-python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_9 --bfold 9 -w 10
+#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_0 --bfold 0 -w 10
+#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_1 --bfold 1 -w 10
+#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_2 --bfold 2 -w 10
+#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_3 --bfold 3 -w 10
+#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_4 --bfold 4 -w 10
+#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_5 --bfold 5 -w 10
+#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_6 --bfold 6 -w 10
+#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_7 --bfold 7 -w 10
+#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_8 --bfold 8 -w 10
+#python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/vimeo.h5 -m merged_traffic/mit/vimeo/fold_9 --bfold 9 -w 10
+
+python wf-attack-vpn/merge_traffic/run_mit.py -app netflix
+#python wf-attack-vpn/merge_traffic/run_mit.py -app rdp
+python wf-attack-vpn/merge_traffic/run_mit.py -app skype
+python wf-attack-vpn/merge_traffic/run_mit.py -app ssh
+#python wf-attack-vpn/merge_traffic/run_mit.py -app vimeo
+#python wf-attack-vpn/merge_traffic/run_mit.py -app voip
+python wf-attack-vpn/merge_traffic/run_mit.py -app youtube
---
+++ merge_traffic/run_mit.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+
+'''
+To run:
+python wf-attack-vpn/merge_traffic/run_mit.py -app ...
+'''
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-app"   , required = True , type = str, default = "", 
+    help="MIT application to use")
+args = vars(ap.parse_args())
+
+def main():
+    '''
+    10-fold merge for an MIT application
+    '''
+
+    app = args["app"]
+
+    for i in range(0,10):
+        os.system(f"python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/{app}.h5 -m merged_traffic/{app}_{i} --bfold {i} -w 10")
+    
+if __name__ == "__main__":
+    main()
