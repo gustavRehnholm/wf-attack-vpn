df4ddcd9e588ee519112d080c4d97c2fcefb2d3b
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-11 12:31:34 +0200

    test 10k packets per file

--- wf-attack/run_all_mit.sh
+++ wf-attack/run_all_mit.sh
@@ -6,12 +6,12 @@ Run DF (with all three modes) on the merged datasets with MIT
 '''
 
 # should take roughly 5 h per 10-fold (total of 35h)
-python wf-attack-vpn/wf-attack/wf_mit.py --app voip
-python wf-attack-vpn/wf-attack/wf_mit.py --app rdp
-python wf-attack-vpn/wf-attack/wf_mit.py --app vimeo
-python wf-attack-vpn/wf-attack/wf_mit.py --app netflix
-python wf-attack-vpn/wf-attack/wf_mit.py --app skype-chat
-python wf-attack-vpn/wf-attack/wf_mit.py --app ssh
-#python wf-attack-vpn/wf-attack/wf_mit.py --app youtube
+python wf-attack-vpn/wf-attack/wf_mit.py --app voip       --len10k True
+python wf-attack-vpn/wf-attack/wf_mit.py --app rdp        --len10k True
+python wf-attack-vpn/wf-attack/wf_mit.py --app vimeo      --len10k True
+python wf-attack-vpn/wf-attack/wf_mit.py --app netflix    --len10k True
+python wf-attack-vpn/wf-attack/wf_mit.py --app skype-chat --len10k True
+python wf-attack-vpn/wf-attack/wf_mit.py --app ssh        --len10k True
+#python wf-attack-vpn/wf-attack/wf_mit.py --app youtube   --len10k True
 
 python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/mit/ -g fig/mit/ -w 10 --ylim_lower 0
--- wf-attack/wf_mit.py
+++ wf-attack/wf_mit.py
@@ -11,6 +11,8 @@ python wf-attack-vpn/wf-attack/wf_mit.py -app ...
 ap = argparse.ArgumentParser()
 ap.add_argument("--app"   , required = True , type = str, default = "", 
     help="MIT application to use")
+ap.add_argument("--len10k"   , required = False , type = Boolean, default = False, 
+    help="How many packets per file to train for")
 args = vars(ap.parse_args())
 
 def main():
@@ -20,10 +22,11 @@ def main():
     print("Start wf attack on 10-fold")
 
     app = args["app"]
+    len10k = args["len10k"]
 
     os.system(f"mkdir wf_result/mit/{app}")
     for i in range(0,10):
-        os.system(f"python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/{app}/fold_{i} -r wf_result/mit/{app}/fold_{i} -s 100 --epochs 30")
+        os.system(f"python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/{app}/fold_{i} -r wf_result/mit/{app}/fold_{i} -s 100 --epochs 30 -l {len10k}")
     
 if __name__ == "__main__":
     main()
