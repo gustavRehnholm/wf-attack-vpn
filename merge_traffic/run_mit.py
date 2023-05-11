8b7c3fb3ba6b0cdb3a3cf602453cf084ce000192
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-11 10:34:53 +0200

    test large file size, for the DF attack

--- merge_traffic/run_mit.py
+++ merge_traffic/run_mit.py
@@ -9,8 +9,10 @@ python wf-attack-vpn/merge_traffic/run_mit.py -app ...
 '''
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-app"   , required = True , type = str, default = "", 
+ap.add_argument("--app"   , required = True , type = str, default = "", 
     help="MIT application to use")
+ap.add_argument("--len"   , required = False , type = int, default = 5000, 
+    help="number of packet per merged file")
 args = vars(ap.parse_args())
 
 def main():
@@ -19,9 +21,10 @@ def main():
     '''
 
     app = args["app"]
+    len = args["len"]
     os.system(f"mkdir merged_traffic/mit/{app}")
     for i in range(0,10):
-        os.system(f"python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/{app}.h5 -m merged_traffic/mit/{app}/fold_{i} --bfold {i} -w 10")
+        os.system(f"python wf-attack-vpn/merge_traffic/main.py -b mit/parsed_app/{app}.h5 -m merged_traffic/mit/{app}/fold_{i} --bfold {i} -w 10 --len {len}")
     
 if __name__ == "__main__":
     main()
