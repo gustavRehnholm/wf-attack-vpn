31f7bbf2c44658bec05661e4654b5cca68fc8c2c
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-11 17:21:05 +0200

    Update wf_mit.py

--- wf-attack/wf_mit.py
+++ wf-attack/wf_mit.py
@@ -22,11 +22,16 @@ def main():
     print("Start wf attack on 10-fold")
 
     app = args["app"]
-    len10k = args["len10k"]
+    
+
+    if args["len10k"]:
+        len10k = "-l"
+    else:
+        len10k = ""
 
     os.system(f"mkdir wf_result/mit/{app}")
     for i in range(0,10):
-        os.system(f"python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/{app}/fold_{i} -r wf_result/mit/{app}/fold_{i} -s 100 --epochs 30 -l {len10k}")
+        os.system(f"python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/{app}/fold_{i} -r wf_result/mit/{app}/fold_{i} -s 100 --epochs 30 {len10k}")
     
 if __name__ == "__main__":
     main()
