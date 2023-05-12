0317b826e5c19880790b2723edefe54e975e4a08
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-12 09:24:16 +0200

    Update wf_mit.py

--- wf-attack/wf_mit.py
+++ wf-attack/wf_mit.py
@@ -31,6 +31,7 @@ def main():
 
     os.system(f"mkdir wf_result/mit/{app}")
     for i in range(0,10):
+        os.system(f"mkdir wf_result/mit/{app}/fold_{i}")
         os.system(f"python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/mit/{app}/fold_{i} -r wf_result/mit/{app}/fold_{i} -s 100 --epochs 30 {len}")
     
 if __name__ == "__main__":
