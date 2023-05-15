696983f03eb36c3a5a06e344cffc7fa077cd9d2e
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-15 14:53:08 +0200

    Update analyze_all_merged.py

--- data_analysis/analyze_all_merged.py
+++ data_analysis/analyze_all_merged.py
@@ -11,27 +11,27 @@ python wf-attack-vpn/data_analysis/analyze_all_merged.py -d merged_traffic -w 10
 '''
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-d"   , required = True , type = str, default = "", 
+ap.add_argument("-i", "--input"   , required = True , type = str, default = "", 
     help="root folder of the merged dataset")
 
 ap.add_argument("-r", "--result", required = True , type = str,
     help="root folder of the overhead result")
 
-ap.add_argument("-w"   , required = False, type = int, default = 10, 
+ap.add_argument("-w", "--workers", required = False, type = int, default = 10, 
     help="number of workers for loading traces from disk")
 
-ap.add_argument("--fold", required = False, type = str, default = "foreground_traffic/fold-0.csv", 
+ap.add_argument("-f", "--fold", required = False, type = str, default = "foreground_traffic/fold-0.csv", 
     help="Path to the fold file to use")
-    
+
 args = vars(ap.parse_args())
 
 def main():
     '''
     Analyze all merged datasets that are in the provided folder
     '''
-    dirs = os.listdir(args["d"])
+    dirs = os.listdir(args["i"])
     for curr_dir in dirs:
-        folder = args['d'] + "/" + curr_dir
+        folder = args['i'] + "/" + curr_dir
         os.mkdir(args['result'])
         fname  = f"{args['result']}/{curr_dir}.txt"
         merged_analysis(dir = folder, workers = args['w'], fold = args["fold"], fname = fname)
