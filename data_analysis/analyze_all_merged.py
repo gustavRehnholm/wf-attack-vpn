5985c7ea38dab81f99b00aebd974d7931a3a53b4
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-15 14:51:14 +0200

    cleanup

--- data_analysis/analyze_all_merged.py
+++ data_analysis/analyze_all_merged.py
@@ -13,12 +13,16 @@ python wf-attack-vpn/data_analysis/analyze_all_merged.py -d merged_traffic -w 10
 ap = argparse.ArgumentParser()
 ap.add_argument("-d"   , required = True , type = str, default = "", 
     help="root folder of the merged dataset")
-ap.add_argument("--result"   , required = True , type = str, default = "", 
+
+ap.add_argument("-r", "--result", required = True , type = str,
     help="root folder of the overhead result")
+
 ap.add_argument("-w"   , required = False, type = int, default = 10, 
     help="number of workers for loading traces from disk")
+
 ap.add_argument("--fold", required = False, type = str, default = "foreground_traffic/fold-0.csv", 
     help="Path to the fold file to use")
+    
 args = vars(ap.parse_args())
 
 def main():
