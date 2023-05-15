f3f69bf107f298fc3ed963748b6a2a02a7fd51a4
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-15 14:18:25 +0200

    cleanup

---
+++ plot_graphs/mit_10k_print.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+'''
+get print for all 10k mit results
+./wf-attack-vpn/plot_graphs/mit_10k_print.sh --app ... -r ... -i ...
+'''
+
+python wf-attack-vpn/plot_graphs/result_10k_print.py --app youtube -r mit_10k_print -i mit_10k
+python wf-attack-vpn/plot_graphs/result_10k_print.py --app netflix -r mit_10k_print -i mit_10k
+python wf-attack-vpn/plot_graphs/result_10k_print.py --app vimeo   -r mit_10k_print -i mit_10k
+
+python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/mit_10k_print/ -g fig/mit_10k/ -w 10 --ylim_lower 0
+
+python wf-attack-vpn/data_analysis/analyze_all_merged.py -d merged_traffic/mit -w 10 --fold foreground_traffic/fold-0.csv
