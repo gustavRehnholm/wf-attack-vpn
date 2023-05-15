5d1002ddd8b8a53f208dc7bf8af8518763506805
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-15 14:33:18 +0200

    set path for the result

--- data_analysis/analyze_all_merged.py
+++ data_analysis/analyze_all_merged.py
@@ -13,6 +13,8 @@ python wf-attack-vpn/data_analysis/analyze_all_merged.py -d merged_traffic -w 10
 ap = argparse.ArgumentParser()
 ap.add_argument("-d"   , required = True , type = str, default = "", 
     help="root folder of the merged dataset")
+ap.add_argument("-r"   , required = True , type = str, default = "", 
+    help="root folder of the overhead result")
 ap.add_argument("-w"   , required = False, type = int, default = 10, 
     help="number of workers for loading traces from disk")
 ap.add_argument("--fold", required = False, type = str, default = "foreground_traffic/fold-0.csv", 
@@ -26,7 +28,8 @@ def main():
     dirs = os.listdir(args["d"])
     for curr_dir in dirs:
         folder = args['d'] + "/" + curr_dir
-        fname  = f"stdout/{curr_dir}.txt"
+        os.mkdir(args['r'])
+        fname  = f"{args['r']}/{curr_dir}.txt"
         merged_analysis(dir = folder, workers = args['w'], fold = args["fold"], fname = fname)
     
 if __name__ == "__main__":
--- plot_graphs/mit_10k_print.sh
+++ plot_graphs/mit_10k_print.sh
@@ -11,4 +11,6 @@ python wf-attack-vpn/plot_graphs/result_10k_print.py --app vimeo   -r mit_10k_pr
 
 python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/mit_10k_print/ -g fig/mit_10k/ -w 10 --ylim_lower 0
 
-python wf-attack-vpn/data_analysis/analyze_all_merged.py -d merged_traffic/mit -w 10 --fold foreground_traffic/fold-0.csv
+python wf-attack-vpn/data_analysis/analyze_all_merged.py -d merged_traffic/mit/youtube -r overhead/youtube_10k -w 10 --fold foreground_traffic/fold-0.csv
+python wf-attack-vpn/data_analysis/analyze_all_merged.py -d merged_traffic/mit/vimeo   -r overhead/vimeo_10k   -w 10 --fold foreground_traffic/fold-0.csv
+python wf-attack-vpn/data_analysis/analyze_all_merged.py -d merged_traffic/mit/netflix -r overhead/netflix_10k -w 10 --fold foreground_traffic/fold-0.csv
