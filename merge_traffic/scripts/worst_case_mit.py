e85e87ea07d93c48d2ece783ddff1b7172d886a5
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-16 07:49:36 +0200

    Update worst_case_mit.py

--- merge_traffic/scripts/worst_case_mit.py
+++ merge_traffic/scripts/worst_case_mit.py
@@ -5,7 +5,7 @@ import os
 
 '''
 To run:
-python wf-attack-vpn/merge_traffic/scripts/worst_case_mit.py --app vimeo
+python wf-attack-vpn/merge_traffic/scripts/worst_case_mit.py --app vimeo -f merged_traffic/worst_case_a -m merged_traffic/worst_case_b
 
 # merge the applications, so that one can be generated for the worst case
 python wf-attack-vpn/merge_traffic/main.py -f netflix -b background_traffic/mit/vimeo.h5 -m merged_traffic/worst_case --bfold  
@@ -13,6 +13,8 @@ python wf-attack-vpn/merge_traffic/main.py -f netflix -b background_traffic/mit/
 
 ap = argparse.ArgumentParser()
 ap.add_argument("--app"   , required = True , type = str, default = "")
+ap.add_argument("-f"   , required = True , type = str, default = "")
+ap.add_argument("-m"   , required = True , type = str, default = "")
 args = vars(ap.parse_args())
 
 def main():
@@ -21,10 +23,13 @@ def main():
     '''
 
     app = args["app"]
+    f_path = args['f']
+    m_path = args['m']
 
     for i in range(0,10):
         os.system(f"mkdir merged_traffic/worst_case/fold_{i}")
-        os.system(f"python wf-attack-vpn/merge_traffic/main.py -f merged_traffic/mit/youtube/fold_{i} -b background_traffic/mit/{app}.h5 -m merged_traffic/worst_case/fold_{i} --bfold {i} --len 10000")
+        #os.system(f"python wf-attack-vpn/merge_traffic/main.py -f merged_traffic/mit/youtube/fold_{i} -b background_traffic/mit/{app}.h5 -m merged_traffic/worst_case/fold_{i} --bfold {i} --len 10000")
+        os.system(f"python wf-attack-vpn/merge_traffic/main.py -f {f_path}/fold_{i} -b background_traffic/mit/{app}.h5 -m {m_path}/fold_{i} --bfold {i} --len 10000")
     
 if __name__ == "__main__":
     main()
