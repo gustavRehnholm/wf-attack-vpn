94c148af30f9c5765629cd47f27b1fffb055ab24
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-17 11:50:25 +0200

    Update run_all_mit.sh

--- wf-attack/run_all_mit.sh
+++ wf-attack/run_all_mit.sh
@@ -16,4 +16,4 @@ python wf-attack-vpn/wf-attack/wf_mit.py --app skype-chat
 python wf-attack-vpn/wf-attack/wf_mit.py --app ssh
 python wf-attack-vpn/wf-attack/wf_mit.py --app youtube
 
-python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/mit/ -g fig/mit/ -w 10 --ylim_lower 0
+python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/mit/ -g fig/mit_5k/ -w 10 --ylim_lower 0
