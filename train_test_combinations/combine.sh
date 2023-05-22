72c737e1a85d43e18bad61fb6eaf182d6bc84afb
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-22 15:09:19 +0200

    destination dir as an input

--- train_test_combinations/combine.sh
+++ train_test_combinations/combine.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # To create all the combinations on how to to test the attacker with access to other background dataset
 
-python wf-attack-vpn/train_test_combinations/combine.py --train foreground_traffic --test merged_traffic/mit_5k/vimeo/fold_5
+# structure deestination merged_traffic/combined/<train set>/<test set>/<fold>
+
+python wf-attack-vpn/train_test_combinations/combine.py --train foreground_traffic --test merged_traffic/mit_5k/vimeo/fold_5 --dest merged_traffic/combined/foreground/vimeo/fold_5
