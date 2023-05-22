e122b7056f5830cdc4d8e9c9465c8a0800b4395b
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-22 17:12:14 +0200

    Update combine_10fold_undef.py

--- train_test_combinations/combine_10fold_undef.py
+++ train_test_combinations/combine_10fold_undef.py
@@ -5,6 +5,7 @@ import os
 import shutil
 from multiprocessing import Pool
 import argparse
+from combine import combine
 
 ap = argparse.ArgumentParser()
 ap.add_argument("--app"   , required = True , type = str, default = "", 
@@ -23,10 +24,15 @@ def combine_10fold(app, f_fold = 0, workers = 10):
         f_fold    - Optional : which fold file to use for the foreground [0, 9]   (int)
         workers   - Optional : number of workers, multiprocessing                 (int)
     '''
-
+    os.system(f"merged_traffic/combined/foreground/{app}")
     for i in range(10):
         print(f"{app}: fold {i}")
-        os.system(f"python wf-attack-vpn/train_test_combinations/combine.py --train foreground_traffic --test merged_traffic/mit_5k/{app}/fold_{i} --dest merged_traffic/combined/foreground/{app}/fold_{i}")
+        os.system(f"merged_traffic/combined/foreground/{app}/fold_{i}")
+        combine(dir_train = "foreground_traffic", 
+                dir_test = f"merged_traffic/mit_5k/{app}/fold_{i}", 
+                dir_dest = f"merged_traffic/combined/foreground/{app}/fold_{i}")
+                
+        #os.system(f"python wf-attack-vpn/train_test_combinations/combine.py --train foreground_traffic --test merged_traffic/mit_5k/{app}/fold_{i} --dest merged_traffic/combined/foreground/{app}/fold_{i}")
 
 if __name__=="__main__":
     main()
