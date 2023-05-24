a0d8027a4bc77380f2e117c1de0d38a0fb96ba64
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-24 17:43:29 +0200

    Update combine_undef_app.py

--- train_test_combinations/undef/combine_undef_app.py
+++ train_test_combinations/undef/combine_undef_app.py
@@ -19,18 +19,19 @@ args = ap.parse_args()
 def main():
 
     # create the new merged dataset
-    ## os.system(f"python wf-attack-vpn/train_test_combinations/combine_10fold_undef.py --app {args.app}")
+    #os.system(f"python wf-attack-vpn/train_test_combinations/undef/combine_10fold_undef.py --app {args.app}")
+
     # WF attack on the new set
-    os.system(f"python wf-attack-vpn/train_test_combinations/combine_10fold_undef_attack.py --app {args.app}")
+    #os.system(f"python wf-attack-vpn/train_test_combinations/undef/combine_10fold_undef_attack.py --app {args.app}")
 
     # print structure
-    #print_struct(dir_input = , dir_result = )
+    print_struct(dir_input = f"wf_result/combined_undef/{args.app}", dir_result = f"wf_result/combined_undef_print/{args.app}")
 
     # test graph
-    #os.system("python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/ -w 10")
+    #os.system(f"python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/combined_undef_print/{args.app} -w 10")
 
     # overhead
-    #os.system("python wf-attack-vpn/data_analysis/analyze_all_merged.py -i merged_traffic -r analyse/...")
+    #os.system("python wf-attack-vpn/data_analysis/merged_analysis/analyze_all_merged.py -i merged_traffic/combined/foreground/{args.app} -r overhead/combine_undef")
 
     # rm the combined dataset
     #os.system(f"rm -f -r {}")
