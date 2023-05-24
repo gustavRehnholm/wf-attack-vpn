e1c88b432ce7af00b9e97a8e1b5151d666a6f60a
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-24 22:27:21 +0200

    setup for longer test

--- train_test_combinations/undef/combine_all_undef.sh
+++ train_test_combinations/undef/combine_all_undef.sh
@@ -6,4 +6,8 @@
 
 # structure deestination merged_traffic/combined/<train set>/<test set>/<fold>
 
-python wf-attack-vpn/train_test_combinations/undef/combine_undef_app.py --app vimeo
+#python wf-attack-vpn/train_test_combinations/undef/combine_undef_app.py --app vimeo
+python wf-attack-vpn/train_test_combinations/undef/combine_undef_app.py --app netflix
+
+
+python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/combined_undef_print -w 10 -g fig/combined_undef
--- train_test_combinations/undef/combine_undef_app.py
+++ train_test_combinations/undef/combine_undef_app.py
@@ -19,23 +19,20 @@ args = ap.parse_args()
 def main():
 
     # create the new merged dataset
-    #os.system(f"python wf-attack-vpn/train_test_combinations/undef/combine_10fold_undef.py --app {args.app}")
+    os.system(f"python wf-attack-vpn/train_test_combinations/undef/combine_10fold_undef.py --app {args.app}")
 
     # WF attack on the new set
-    #os.system(f"python wf-attack-vpn/train_test_combinations/undef/combine_10fold_undef_attack.py --app {args.app}")
+    os.system(f"python wf-attack-vpn/train_test_combinations/undef/combine_10fold_undef_attack.py --app {args.app}")
 
     # print structure
-    #print_struct(dir_input = f"wf_result/combined_undef/{args.app}", dir_result = f"wf_result/combined_undef_print/{args.app}")
-
-    # test graph
-    # python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/combined_undef_print/vimeo -w 10 -g fig/combined_undef
-    os.system(f"python wf-attack-vpn/plot_graphs/plot_all.py -r wf_result/combined_undef_print/{args.app} -w 10")
+    print_struct(dir_input = f"wf_result/combined_undef/{args.app}", dir_result = f"wf_result/combined_undef_print/{args.app}")
 
     # overhead
-    #os.system("python wf-attack-vpn/data_analysis/merged_analysis/analyze_all_merged.py -i merged_traffic/combined/foreground/{args.app} -r overhead/combine_undef")
+    # python wf-attack-vpn/data_analysis/merged_analysis/analyze_all_merged.py -i merged_traffic/combined/foreground/vimeo -r overhead/combine_undef
+    os.system(f"python wf-attack-vpn/data_analysis/merged_analysis/analyze_all_merged.py -i merged_traffic/combined/foreground/{args.app} -r overhead/combine_undef")
 
     # rm the combined dataset
-    #os.system(f"rm -f -r {}")
+    os.system(f"rm -f -r merged_traffic/combined/foreground/{args.app}")
 
 if __name__ == "__main__":
     main()
