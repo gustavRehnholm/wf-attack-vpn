11d2da6b16374d04d90fb2b2f2d9e6e787410e0c
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-25 12:35:48 +0200

    path correction

--- train_test_combinations/undef/combine_10fold_undef_attack.py
+++ train_test_combinations/undef/combine_10fold_undef_attack.py
@@ -1,28 +1,15 @@
 #!/usr/bin/env python3
 
-import argparse
 import os
 
-'''
-To run:
-python wf-attack-vpn/wf-attack/train_test_combinations/combine_wf_attack.py --app vimeo
-'''
-
-ap = argparse.ArgumentParser()
-
-ap.add_argument("--app"   , required = True , type = str, default = "", 
-    help="Which dataset (which uses the application) should be attacked")
-
-args = ap.parse_args()
-
-def main():
+def combine_10fold_undef_attack(app):
 
     print("Start wf attack on combined 10-fold")
 
-    os.system(f"mkdir wf_result/combine_undef/{args.app}")
+    os.system(f"mkdir wf_result/combined_undef/{app}")
     for i in range(0,10):
-        os.system(f"mkdir wf_result/combine_undef/{args.app}/fold_{i}")
-        os.system(f"python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/combined/foreground/{args.app}/fold_{i}/client -r wf_result/combine_undef/{args.app}/fold_{i} -s 100 --epochs 30")
+        os.system(f"mkdir wf_result/combined_undef/{app}/fold_{i}")
+        os.system(f"python wf-attack-vpn/wf-attack/wf-attack-dir.py -m merged_traffic/combined/foreground/{app}/fold_{i}/client -r wf_result/combined_undef/{app}/fold_{i} -s 100 --epochs 30")
     
 if __name__ == "__main__":
     main()
--- train_test_combinations/undef/combine_undef_app.py
+++ train_test_combinations/undef/combine_undef_app.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+from combine_10fold_undef_attack import combine_10fold_undef_attack
 from print_struct import print_struct
 
 '''
@@ -19,10 +20,10 @@ args = ap.parse_args()
 def main():
 
     # create the new merged dataset
-    os.system(f"python wf-attack-vpn/train_test_combinations/undef/combine_10fold_undef.py --app {args.app}")
+    # os.system(f"python wf-attack-vpn/train_test_combinations/undef/combine_10fold_undef.py --app {args.app}")
 
     # WF attack on the new set
-    os.system(f"python wf-attack-vpn/train_test_combinations/undef/combine_10fold_undef_attack.py --app {args.app}")
+    combine_10fold_undef_attack(args.app)
 
     # print structure
     print_struct(dir_input = f"wf_result/combined_undef/{args.app}", dir_result = f"wf_result/combined_undef_print/{args.app}")
@@ -31,7 +32,7 @@ def main():
     #os.system(f"python wf-attack-vpn/data_analysis/merged_analysis/analyze_all_merged.py -i merged_traffic/combined/foreground/{args.app} -r overhead/combine_undef/{args.app}")
 
     # rm the combined dataset
-    os.system(f"rm -f -r merged_traffic/combined/foreground/{args.app}")
+    # os.system(f"rm -f -r merged_traffic/combined/foreground/{args.app}")
 
 if __name__ == "__main__":
     main()
