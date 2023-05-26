b730ca6f78f686d350f3203f0180fe1bce1f0fc0
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-26 19:02:15 +0200

    Update combine_all.sh

--- train_test_combinations/merged/combine_all.sh
+++ train_test_combinations/merged/combine_all.sh
@@ -2,7 +2,7 @@
 # To create all the combinations on how to to test the attacker with access to other background dataset
 
 # to run:
-# ./wf-attack-vpn/train_test_combinations/undef/combine_all_undef.sh
+# ./wf-attack-vpn/train_test_combinations/merged/combine_all.sh
 
 # structure deestination merged_traffic/combined/<train set>/<test set>/<fold>
 
