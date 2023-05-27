5ed736daa17ea0f10eb3af9b83f3017b64060ed7
gustavRehnholm <55880507+gustavRehnholm@users.noreply.github.com>
2023-05-27 11:40:20 +0200

    Create twitch_10fold.sh

---
+++ merge_traffic/scripts/twitch_10fold.sh
@@ -0,0 +1,12 @@
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_1_middle.h5 -m merged_traffic/twitch_div_rnd_1_middle/fold_0  --ffold 0 --bfold 0 -w 5 --div True
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_1_middle.h5 -m merged_traffic/twitch_div_rnd_1_middle/fold_1  --ffold 0 --bfold 1 -w 5 --div True
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_1_middle.h5 -m merged_traffic/twitch_div_rnd_1_middle/fold_2  --ffold 0 --bfold 2 -w 5 --div True
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_1_middle.h5 -m merged_traffic/twitch_div_rnd_1_middle/fold_3  --ffold 0 --bfold 3 -w 5 --div True
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_1_middle.h5 -m merged_traffic/twitch_div_rnd_1_middle/fold_4  --ffold 0 --bfold 4 -w 5 --div True
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_1_middle.h5 -m merged_traffic/twitch_div_rnd_1_middle/fold_5  --ffold 0 --bfold 5 -w 5 --div True
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_1_middle.h5 -m merged_traffic/twitch_div_rnd_1_middle/fold_6  --ffold 0 --bfold 6 -w 5 --div True
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_1_middle.h5 -m merged_traffic/twitch_div_rnd_1_middle/fold_7  --ffold 0 --bfold 7 -w 5 --div True
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_1_middle.h5 -m merged_traffic/twitch_div_rnd_1_middle/fold_8  --ffold 0 --bfold 8 -w 5 --div True
+python wf-attack-vpn/merge_traffic/main.py -f foreground_traffic -b background_traffic/twitch_1_middle.h5 -m merged_traffic/twitch_div_rnd_1_middle/fold_9  --ffold 0 --bfold 9 -w 5 --div True
+
+
